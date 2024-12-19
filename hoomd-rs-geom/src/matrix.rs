4e9865b4432cae7522fdfdabf711d7c6c041ec96
janbridley <bradleyjenj@gmail.com>
2024-12-19 11:03:57 -0500

    Progress on Matrix

--- hoomd-rs-geom/src/matrix.rs
+++ hoomd-rs-geom/src/matrix.rs
@@ -1,5 +1,6 @@
 use hoomd_rs_vector::vector::Cartesian;
-use std::ops::Index;
+use std::ops::{Add, Index};
+use std::array;
 
 pub trait Matrix<const M: usize, const N: usize> {
     fn shape(&self) -> [usize; 2] {
@@ -21,14 +22,14 @@ pub trait Det<const N: usize>: SquareMatrix<N> {
 #[derive(Debug)]
 pub struct ArrayXX<const N: usize, const M: usize> {
     // M rows and N columns
-    coordinates: [Cartesian<N>; M],
+    rows: [Cartesian<N>; M],
 }
 
 impl<const N: usize, const M: usize> Index<usize> for ArrayXX<N, M> {
     type Output = Cartesian::<N>;
 
     fn index(&self, i: usize) -> &Self::Output {
-        &self.coordinates[i]
+        &self.rows[i]
     }
 }
 
@@ -37,12 +38,28 @@ impl<const N: usize, const M: usize> Index<usize> for ArrayXX<N, M> {
 impl<const N: usize, const M: usize> Matrix<N, M> for ArrayXX<N, M> {}
 impl<const N: usize> SquareMatrix<N> for ArrayXX<N, N> {}
 
+impl<const N: usize> ArrayXX<N, N> {
+    fn diag(&self, k: usize) -> Cartesian::<N>
+    {
+        array::from_fn(|i| (*self)[i].coordinates[i+k]).into()
+    }
+
+}
+
 impl Det<2> for ArrayXX<2, 2> {
     fn det(&self) -> f64 where Self: Sized {
-        let m = self.coordinates;
-        m[0][0]*m[1][1] - m[0][1]*m[1][0]
+        self[0].coordinates[0]*self[1].coordinates[1] - self[0].coordinates[1]*self[1].coordinates[0]
     }
 }
+
+impl Det<3> for ArrayXX<3, 3> {
+    fn det(&self) -> f64 where Self: Sized {
+        self.diag(0).coordinates.iter().product::<f64>() + 
+        self[0].coordinates[1] * self[1].coordinates[2] * self[2].coordinates[0]
+        self[0].coordinates[2] * self[1].coordinates[0] * self[2].coordinates[0]
+    }
+}
+
 
 // impl<const N: usize, const M: usize> ArrayXX<N, M>
 //     where
