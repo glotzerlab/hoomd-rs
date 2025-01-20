d5eb5d6d2da3122840f16597f8a48475ae9f7cee
janbridley <bradleyjenj@gmail.com>
2025-01-20 14:16:57 -0500

    Clean up and lint

--- hoomd-interaction/src/pairwise/xplor.rs
+++ hoomd-interaction/src/pairwise/xplor.rs
@@ -17,45 +17,47 @@ pub struct Xplor<F> {
     pub f: F,
     /// `r` value `[length]` where the smoothed potential will be 0.
     pub r_cut: f64,
-    /// `r` value `[length]` where the smoothing function is enabled. Should be < r_cut
+    /// `r` value `[length]` where the smoothing function is enabled. Should be < `r_cut`
     pub r_on: f64, // TODO: find alternate name?
 }
 
-impl<F> Xplor<F>{
+impl<F> Xplor<F> {
     #[inline]
     #[must_use]
     pub fn new(f: F, r_cut: f64, r_on: f64) -> Self {
         Self { f, r_cut, r_on }
     }
 
-  /// The xplor shifting function
-  #[inline]
-  fn s(&self, r: f64) -> f64 {
-    if r < self.r_on {
-      1.0
-    } else if r > self.r_cut {
-      0.0 
-    } else {
-      let r_sq = r.powi(2);
-      let r_cut_sq = self.r_cut.powi(2);
-      let r_on_sq = self.r_on.powi(2);
-      (r_cut_sq - r_sq).powi(2) * (r_cut_sq + 2.0 * r_sq-3.0 * r_on_sq) / (r_cut_sq - r_on_sq).powi(3)
+    /// The xplor shifting function
+    #[inline]
+    fn s(&self, r: f64) -> f64 {
+        if r < self.r_on {
+            1.0
+        } else if r > self.r_cut {
+            0.0
+        } else {
+            let r_sq = r.powi(2);
+            let r_cut_sq = self.r_cut.powi(2);
+            let r_on_sq = self.r_on.powi(2);
+            (r_cut_sq - r_sq).powi(2) * (r_cut_sq + 2.0 * r_sq - 3.0 * r_on_sq)
+                / (r_cut_sq - r_on_sq).powi(3)
+        }
     }
-  }
-  /// The xplor shifting function
-  #[inline]
-  fn ds_dr(&self, r: f64) -> f64 { // TODO: can we share r_*sq between energy and force?
-    if r < self.r_on {
-      1.0 // TODO: is this still correct? I think not?
-    } else if r > self.r_cut {
-      0.0 
-    } else {
-      let r_sq = r.powi(2);
-      let r_cut_sq = self.r_cut.powi(2);
-      let r_on_sq = self.r_on.powi(2);
-      12.0 * r * ((r_cut_sq - r_sq)*(r_on_sq - r_sq)) / (r_cut_sq - r_on_sq).powi(3)
+    /// The xplor shifting function
+    #[inline]
+    fn ds_dr(&self, r: f64) -> f64 {
+        // TODO: can we share r_*sq between energy and force?
+        if r < self.r_on {
+            1.0 // TODO: is this still correct? I think not?
+        } else if r > self.r_cut {
+            0.0
+        } else {
+            let r_sq = r.powi(2);
+            let r_cut_sq = self.r_cut.powi(2);
+            let r_on_sq = self.r_on.powi(2);
+            12.0 * r * ((r_cut_sq - r_sq) * (r_on_sq - r_sq)) / (r_cut_sq - r_on_sq).powi(3)
+        }
     }
-  }
 }
 
 impl<F: IsotropicEnergy> IsotropicEnergy for Xplor<F> {
@@ -66,7 +68,7 @@ impl<F: IsotropicEnergy> IsotropicEnergy for Xplor<F> {
         self.s(r) * self.f.energy(r)
     }
     // NOTE: HOOMD impl has special case where r_on > r_cut. However, because each pot
-    // is seperate in this version, users can just not xplor on WCA
+    // is separate in this version, users can just not xplor on WCA
 }
 
 impl<F: IsotropicForce + IsotropicEnergy> IsotropicForce for Xplor<F> {
@@ -91,7 +93,6 @@ mod tests {
         #[values(1.0, 2.0, 0.5)] sigma: f64,
     ) {
         let lj: LennardJones = LennardJones::new(epsilon, sigma);
-        println!("{}, {}", lj.sigma, lj.epsilon);
         let r_on = 1.0; // Provides cases where r_on <, =, > sigma and epsilon
         let r_cut = 2.5 * sigma;
         let xplor_lj = Xplor::new(lj, r_cut, r_on);
@@ -102,29 +103,29 @@ mod tests {
         assert_eq!(xplor_lj.r_cut, r_cut);
 
         // Values should not be shifted below r_on
-        assert_abs_diff_eq!(xplor_lj.energy(r_on/2.0), lj.energy(r_on/2.0));
-        assert_abs_diff_eq!(xplor_lj.energy(r_on-1e-6), lj.energy(r_on-1e-6));
+        assert_abs_diff_eq!(xplor_lj.energy(r_on / 2.0), lj.energy(r_on / 2.0));
+        assert_abs_diff_eq!(xplor_lj.energy(r_on - 1e-6), lj.energy(r_on - 1e-6));
         // assert_abs_diff_eq!(xplor_lj.force(r_on/2.0), lj.force(r_on/2.0)); // TODO
         // assert_abs_diff_eq!(xplor_lj.force(r_on-1e-6), lj.force(r_on-1e-6)); // TODO
 
         // Values should be zero at and above r_cut
         assert_abs_diff_eq!(xplor_lj.energy(r_cut), 0.0);
-        assert_abs_diff_eq!(xplor_lj.energy(r_cut+1e-6), 0.0);
-        assert_abs_diff_eq!(xplor_lj.energy(r_cut*2.0), 0.0);
+        assert_abs_diff_eq!(xplor_lj.energy(r_cut + 1e-6), 0.0);
+        assert_abs_diff_eq!(xplor_lj.energy(r_cut * 2.0), 0.0);
 
         assert_abs_diff_eq!(xplor_lj.force(r_cut), 0.0); // TODO: should be zero!
-        assert_abs_diff_eq!(xplor_lj.force(r_cut+1e-6), 0.0);
-        assert_abs_diff_eq!(xplor_lj.force(r_cut*2.0), 0.0);
+        assert_abs_diff_eq!(xplor_lj.force(r_cut + 1e-6), 0.0);
+        assert_abs_diff_eq!(xplor_lj.force(r_cut * 2.0), 0.0);
 
         assert_abs_diff_eq!(xplor_lj.energy(sigma), 0.0);
         // assert_relative_eq!(xplor_lj.force(sigma), 24.0 * epsilon / sigma);
 
         // Values should not be the same between r_on and r_cut
-        assert_abs_diff_ne!(xplor_lj.energy(r_on+1e-4), lj.energy(r_on+1e-4));
-        
+        assert_abs_diff_ne!(xplor_lj.energy(r_on + 1e-4), lj.energy(r_on + 1e-4));
+
         // Zero crossing
         assert_abs_diff_eq!(xplor_lj.energy(sigma), 0.0);
-        // assert_relative_eq!(xplor_lj.force(sigma), 24.0 * epsilon / sigma); // TODO 
+        // assert_relative_eq!(xplor_lj.force(sigma), 24.0 * epsilon / sigma); // TODO
 
         // Bottom of the well
         let r_min = 2.0_f64.powf(1.0 / 6.0) * sigma;
@@ -134,7 +135,7 @@ mod tests {
         // r = 2 sigma
         assert_relative_eq!(
             xplor_lj.energy(2.0 * sigma),
-            -63.0 / 1024.0 * epsilon * xplor_lj.s(2.0*sigma)
+            -63.0 / 1024.0 * epsilon * xplor_lj.s(2.0 * sigma)
         );
         // assert_relative_eq!(
         //     xplor_lj.force(2.0 * sigma),
