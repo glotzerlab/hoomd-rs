ebfa7b5019cc385face9912114e540b22f879a59
janbridley <bradleyjenj@gmail.com>
2025-01-23 19:05:54 -0500

    Pre-commit fixes

--- hoomd-geom/src/matrix.rs
+++ hoomd-geom/src/matrix.rs
@@ -1,5 +1,5 @@
-// // Copyright (c) 2024-2025 The Regents of the University of Michigan.
-// // Part of hoomd-rs, released under the BSD 3-Clause License.
+// Copyright (c) 2024-2025 The Regents of the University of Michigan.
+// Part of hoomd-rs, released under the BSD 3-Clause License.
 
 // use hoomd_vector::Cartesian;
 // use std::array;
