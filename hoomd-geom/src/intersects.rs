25753a79e893d1724d1b33e4eb15a38fdc028a4a
janbridley <55467578+janbridley@users.noreply.github.com>
2025-02-04 11:27:21 -0500

    Add intersects_at

--- hoomd-geom/src/intersects.rs
+++ hoomd-geom/src/intersects.rs
@@ -2,11 +2,24 @@
 // Part of hoomd-rs, released under the BSD 3-Clause License.
 
 use crate::Sphere;
-use hoomd_vector::{Cartesian, Vector};
+use hoomd_vector::{Cartesian, Vector, Rotate};
 // use crate::Shape; // TODO: do we want this as a trait bound on S?
 
+/**
+Define a position and orientation-independent intersection based solely on the geometry
+of the shape.
+*/
 pub trait Intersects<S> {
+    /// Determine whether a Shape intersects another shape (based on intrinsic location).
     fn intersects(&self, other: S) -> bool;
+}
+
+/**
+Define a position and orientation-dependent intersection between two bodies.
+*/
+pub trait IntersectsAt<S, V: Vector, R: Rotate<V>> {
+    ///Determine whether a Particle intersects another shape at some position and orientation.
+    fn intersects_at(&self, other:S, r_ij: &V, o_ij: &R) -> bool;
 }
 
 /// Whether a shape is fully bounded by another
