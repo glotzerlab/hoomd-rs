c238bdec8c3ceb8dc085dc40c79d24e86640e17a
Domagoj Fijan <domagoj.fijan@gmail.com>
2025-03-04 16:10:24 -0500

    add basic docstrings

--- hoomd-spatial/src/lib.rs
+++ hoomd-spatial/src/lib.rs
@@ -7,19 +7,28 @@
 #![doc(
     html_logo_url = "https://hoomd-blue.readthedocs.io/en/latest/_static/hoomdblue-logo-favicon.svg"
 )]
-/*! TODO DOCS */
+/*! Implements spatial data structures for efficient neighbor finding.
+ */
 
 use hoomd_vector::Cartesian;
 use std::collections::HashMap;
 
+/** For ghost particles
+**/
+#[non_exhaustive]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum ParticleFlag {
-    Real,
-    Ghost,
+    // Real particles
+    Real = 0,
+    // Ghost particles
+    Ghost = 1,
 }
 
+/** Cell list docs. */
 pub struct CellList<const N: usize> {
+    /// The width of each cell.
     pub cell_width: f64,
+    /// A map from cell indices to particle indices.
     pub cell_idx_to_particle_indices: HashMap<[isize; N], Vec<usize>>,
     /// A map from particle indices to cell indices.
     pub particle_idx_to_cell_index: HashMap<usize, [isize; N]>,
@@ -28,11 +37,33 @@ pub struct CellList<const N: usize> {
 }
 
 impl<const N: usize> CellList<N> {
+    /** A helper function which converts given positions to cell indices.
+     */
     #[inline]
     fn cell_index_from_position(cell_width: f64, position: &Cartesian<N>) -> [isize; N] {
         std::array::from_fn(|j| (position.coordinates[j] / cell_width).floor() as isize)
     }
 
+    /** Create a new cell list from the given cell width and positions.
+
+    # Example
+
+    ```
+    use hoomd_spatial::CellList;
+    use hoomd_vector::Cartesian;
+
+    // Create some sample 2D Cartesian positions.
+    let positions = vec![
+        Cartesian { coordinates: [0.2, 0.3] },
+        Cartesian { coordinates: [0.8, 1.3] },
+        Cartesian { coordinates: [8.5, 9.5] },
+    ];
+    let cell_width = 1.0;
+    // Build the cell list from positions.
+    let cell_list = CellList::<2>::new(cell_width, &positions);
+    ```
+    */
+    #[inline]
     pub fn new(cell_width: f64, positions: &Vec<Cartesian<N>>) -> Self {
         let mut instance = Self {
             cell_width,
@@ -48,10 +79,58 @@ impl<const N: usize> CellList<N> {
         instance
     }
 
+    /** Returns a cell index (in form of a tuple) for a given particle index.
+
+    # Example
+
+    ```
+    use hoomd_spatial::CellList;
+    use hoomd_vector::Cartesian;
+
+    // Create some sample 2D Cartesian positions.
+    let positions = vec![
+        Cartesian { coordinates: [0.2, 0.3] },
+        Cartesian { coordinates: [0.8, 1.3] },
+        Cartesian { coordinates: [8.5, 9.5] },
+    ];
+    let cell_width = 1.0;
+    // Build the cell list from positions.
+    let cell_list = CellList::<2>::new(cell_width, &positions);
+
+    // Get the cell index for the first particle.
+    let cell_index = cell_list.cell_index_from_particle_index(0).unwrap();
+    ```
+     */
+    #[inline]
+    #[must_use]
     pub fn cell_index_from_particle_index(&self, particle_index: usize) -> Option<&[isize; N]> {
         self.particle_idx_to_cell_index.get(&particle_index)
     }
 
+    /** Add particle to the cell list.
+
+    # Example
+
+    ```
+    use hoomd_spatial::CellList;
+    use hoomd_vector::Cartesian;
+
+    // Create some sample 2D Cartesian positions.
+    let positions = vec![
+        Cartesian { coordinates: [0.2, 0.3] },
+        Cartesian { coordinates: [0.8, 1.3] },
+        Cartesian { coordinates: [8.5, 9.5] },
+    ];
+    let cell_width = 1.0;
+    // Build the cell list from positions.
+    let mut cell_list = CellList::<2>::new(cell_width, &positions);
+
+    // Add a new particle to the cell list.
+    let new_position = Cartesian { coordinates: [1.2, 1.3] };
+    cell_list.add_particle(&new_position);
+    ```
+    */
+    #[inline]
     pub fn add_particle(&mut self, position: &Cartesian<N>) {
         let particle_index = self.particle_max_index;
         let cell_idx = Self::cell_index_from_position(self.cell_width, position);
@@ -63,6 +142,35 @@ impl<const N: usize> CellList<N> {
             .insert(particle_index, cell_idx);
     }
 
+    /** Remove particle from the cell list.
+
+    Note that removing a particle from the cell list will not change the maximum particle index.
+
+    # Example
+
+    ```
+    use hoomd_spatial::CellList;
+    use hoomd_vector::Cartesian;
+
+    // Create some sample 2D Cartesian positions.
+    let positions = vec![
+        Cartesian { coordinates: [0.2, 0.3] },
+        Cartesian { coordinates: [0.8, 1.3] },
+        Cartesian { coordinates: [8.5, 9.5] },
+    ];
+    let cell_width = 1.0;
+    // Build the cell list from positions.
+    let mut cell_list = CellList::<2>::new(cell_width, &positions);
+
+    // Remove the first particle from the cell list.
+    cell_list.remove_particle(0);
+    ```
+
+    # Panics
+
+    This function will panic if the particle index is not found in the cell list.
+    */
+    #[inline]
     pub fn remove_particle(&mut self, particle_index: usize) {
         //  unwrap will panic if particle index is not present
         // TODO think about using entry_and_modify to make this code cleaner
@@ -81,6 +189,34 @@ impl<const N: usize> CellList<N> {
         }
     }
 
+    /** Translate particle to a new position.
+
+    # Example
+
+    ```
+    use hoomd_spatial::CellList;
+    use hoomd_vector::Cartesian;
+
+    // Create some sample 2D Cartesian positions.
+    let positions = vec![
+        Cartesian { coordinates: [0.2, 0.3] },
+        Cartesian { coordinates: [0.8, 1.3] },
+        Cartesian { coordinates: [8.5, 9.5] },
+    ];
+    let cell_width = 1.0;
+    // Build the cell list from positions.
+    let mut cell_list = CellList::<2>::new(cell_width, &positions);
+
+    // Translate the first particle to a new position.
+    let new_position = Cartesian { coordinates: [1.2, 1.3] };
+    cell_list.translate_particle(0, new_position);
+    ```
+
+    # Panics
+
+    This function will panic if the particle index is not found in the cell list.
+    */
+    #[inline]
     pub fn translate_particle(
         &mut self,
         particle_index: usize,
