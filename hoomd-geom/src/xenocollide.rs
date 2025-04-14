b0933101410f7609df0ea4aa90c13f97c0b9871f
janbridley <bradleyjenj@gmail.com>
2025-04-14 14:24:47 -0400

    Run fix-license-header

--- hoomd-geom/src/xenocollide.rs
+++ hoomd-geom/src/xenocollide.rs
@@ -1,3 +1,6 @@
+// Copyright (c) 2024-2025 The Regents of the University of Michigan.
+// Part of hoomd-rs, released under the BSD 3-Clause License.
+
 use crate::{IntersectsAt, Shape, SupportFn, Volume};
 use hoomd_vector::{Angle, Cartesian, Cross, Rotate, Rotation, Vector};
 
