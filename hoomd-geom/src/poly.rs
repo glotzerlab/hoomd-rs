46e9e29a8ca562045f8c51f7a175dc3d1ebf3503
janbridley <bradleyjenj@gmail.com>
2025-04-15 15:34:56 -0400

    Add ConvexPolytope<2>::from(usize)

--- hoomd-geom/src/poly.rs
+++ hoomd-geom/src/poly.rs
@@ -1,7 +1,7 @@
 // Copyright (c) 2024-2025 The Regents of the University of Michigan.
 // Part of hoomd-rs, released under the BSD 3-Clause License.
 
-use crate::{IntersectsAt, Shape, SupportFn, Volume, xenocollide};
+use crate::{xenocollide, IntersectsAt, Shape, SupportFn, Volume};
 use hoomd_vector::{Cartesian, Rotate, Rotation, Vector};
 
 /**
@@ -9,7 +9,7 @@ A convex, faceter polyhedron
 */
 pub struct ConvexPolytope<const N: usize> {
     /// The vertices of the shape.
-    vertices: Vec<Cartesian<N>>,
+    pub vertices: Vec<Cartesian<N>>,
     // rounding_radius: f64,
     // minimal_centered_bounding_sphere_radius: f64,
 }
@@ -26,8 +26,32 @@ where
     }
 }
 
+impl From<usize> for ConvexPolytope<2> {
+    /** Create a regular N-gon with N vertices and circumradius one.
+
+    # Example
+    ```
+    use hoomd_geom::poly::ConvexPolytope;
+
+    let equilateral_triangle = ConvexPolytope::from(3);
+    ```
+    */
+    #[inline]
+    fn from(n: usize) -> ConvexPolytope<2> {
+        ConvexPolytope {
+            vertices: (0..n)
+                .map(|x| {
+                    let theta = std::f64::consts::PI * (x as f64) / (n as f64);
+                    Cartesian::from([f64::cos(theta), f64::cos(theta)])
+                })
+                .collect::<Vec<_>>(),
+        }
+    }
+}
+
 #[allow(clippy::expect_used)]
 impl<const N: usize> SupportFn<Cartesian<N>> for ConvexPolytope<N> {
+    #[inline]
     fn support(&self, n: &Cartesian<N>) -> Cartesian<N> {
         *self
             .vertices
@@ -48,19 +72,5 @@ impl<R: Rotate<Cartesian<3>>> IntersectsAt<Self, Cartesian<3>, R> for ConvexPoly
     ///
     fn intersects_at(&self, other: &Self, r_ij: &Cartesian<3>, o_ij: &R) -> bool {
         todo!() // TODO: Xenocollide 3d
-    }
-}
-
-impl ConvexPolytope<2> {
-    /// Xeonocolloide 2d
-    fn mpr<R: Rotate<Cartesian<2>>>(&self, other: &Self, r_ij: &Cartesian<2>, o_ij: &R) -> bool {
-        // 1a: Determine whether the origin lies in B⊖A, given only the support mapping
-        // 1b: Obtain a point that lies deep in B⊖A:
-        let p = r_ij; // self.centroid()-other.centroid() in extrinsic coords
-
-        // 1c: Construct a normal pointing from p to the origin: this is just p̂?
-        // Find support point in this direction
-
-        false
     }
 }
