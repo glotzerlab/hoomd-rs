5e94e128d3ae2379dd894f181a87895769374330
janbridley <bradleyjenj@gmail.com>
2025-04-16 10:52:57 -0400

    Add 3d benchmarks

--- hoomd-geom/benches/overlaps.rs
+++ hoomd-geom/benches/overlaps.rs
@@ -8,11 +8,15 @@
 
 use divan::counter::ItemsCount;
 use divan::{self, black_box, Bencher};
-use hoomd_geom::{poly::ConvexPolytope, xenocollide::collide2d, Cuboid, IntersectsAt, Sphere};
+use hoomd_geom::{
+    poly::ConvexPolytope,
+    xenocollide::{collide2d, collide3d},
+    Cuboid, IntersectsAt, Sphere,
+};
 use rand::distributions::Uniform;
 use rand::{rngs::StdRng, Rng, SeedableRng};
 
-use hoomd_vector::{Angle, Cartesian, Rotate, Rotation, RotationMatrix};
+use hoomd_vector::{Angle, Cartesian, Rotate, Rotation, RotationMatrix, Versor};
 
 fn main() {
     divan::main();
@@ -20,14 +24,14 @@ fn main() {
 
 fn create_sphere_pair<const N: usize, R: Rng>(rng: &mut R) -> (Sphere<N>, Sphere<N>) {
     (
-        Sphere::from(rng.gen_range(0.0..100.0)),
-        Sphere::from(rng.gen_range(0.0..100.0)),
+        Sphere::from(rng.gen_range(0.0..10.0)),
+        Sphere::from(rng.gen_range(0.0..10.0)),
     )
 }
 fn create_cuboid_pair<const N: usize, R: Rng>(rng: &mut R) -> (Cuboid<N>, Cuboid<N>) {
     (
-        Cuboid::from(rng.gen::<Cartesian<N>>() * 100.0),
-        Cuboid::from(rng.gen::<Cartesian<N>>() * 100.0),
+        Cuboid::from(rng.gen::<Cartesian<N>>() * 10.0),
+        Cuboid::from(rng.gen::<Cartesian<N>>() * 10.0),
     )
 }
 
@@ -37,24 +41,30 @@ fn create_polygon_pair<const N: usize>() -> (ConvexPolytope<2>, ConvexPolytope<2
 
 fn create_offset_2d<R: Rng>(rng: &mut R) -> (Cartesian<2>, Angle) {
     (
-        rng.gen::<Cartesian<2>>() * 100.0,
+        rng.gen::<Cartesian<2>>() * 10.0,
         Angle::from(rng.gen_range((-2.0 * std::f64::consts::PI)..(2.0 * std::f64::consts::PI))),
     )
 }
+fn create_offset<const N: usize, R: Rng>(rng: &mut R) -> (Cartesian<N>, RotationMatrix<N>) {
+    (rng.gen::<Cartesian<N>>() * 10.0, RotationMatrix::default())
+}
+fn create_offset_3d<R: Rng>(rng: &mut R) -> (Cartesian<3>, Versor) {
+    (rng.gen::<Cartesian<3>>() * 10.0, rng.gen())
+}
 
-// const DIMENSIONS: &[usize] = &[1, 2, 3, 4];
+const DIMENSIONS: &[usize] = &[2, 3, 4];
 const NUM_VERTICES: &[usize] = &[3, 8, 16, 64, 256];
 
-#[divan::bench]
-fn sphere_fast_2d(bencher: Bencher) {
+#[divan::bench(consts = DIMENSIONS)]
+fn sphere_fast_nd<const N: usize>(bencher: Bencher) {
     let mut rng = StdRng::seed_from_u64(1);
 
     bencher
         .counter(ItemsCount::from(1_u32))
         .with_inputs(|| {
             (
-                create_sphere_pair::<2, _>(&mut rng),
-                create_offset_2d(&mut rng),
+                create_sphere_pair::<N, _>(&mut rng),
+                create_offset::<N, _>(&mut rng),
             )
         })
         .bench_local_values(|((s0, s1), (t, r))| black_box(s0.intersects_at(&s1, &t, &r)));
@@ -76,7 +86,7 @@ fn cuboid_fast_2d(bencher: Bencher) {
         .bench_local_values(|((c0, c1), (t, _), r)| black_box(c0.intersects_at(&c1, &t, &r)));
 }
 
-#[divan::bench()]
+#[divan::bench]
 fn sphere_xenocollide_2d(bencher: Bencher) {
     let mut rng = StdRng::seed_from_u64(1);
 
@@ -91,8 +101,23 @@ fn sphere_xenocollide_2d(bencher: Bencher) {
         .bench_local_values(|((s0, s1), (t, r))| black_box(collide2d(&s0, &s1, &t, &r)));
 }
 
+#[divan::bench]
+fn sphere_xenocollide_3d(bencher: Bencher) {
+    let mut rng = StdRng::seed_from_u64(1);
+
+    bencher
+        .counter(ItemsCount::from(1_u32))
+        .with_inputs(|| {
+            (
+                create_sphere_pair::<3, _>(&mut rng),
+                create_offset_3d(&mut rng),
+            )
+        })
+        .bench_local_values(|((s0, s1), (t, r))| black_box(collide3d(&s0, &s1, &t, &r)));
+}
+
 /// Note this is not 1:1 with the naive test, as this uses oriented cuboids!
-#[divan::bench()]
+#[divan::bench]
 fn cuboid_xenocollide_2d(bencher: Bencher) {
     let mut rng = StdRng::seed_from_u64(1);
 
