2c2b0418a6a3f7150a5995e6818c807f057f8869
janbridley <bradleyjenj@gmail.com>
2025-04-19 14:22:45 -0400

    Notes on tt.clj

--- hoomd-geom/tt.clj
+++ hoomd-geom/tt.clj
@@ -87,17 +87,43 @@
   "Takes 2 bitmasks and edge flags, returns true if there's a
   separating plane between the faces shared by that edge."
   [ma mb ea eb]
+  ; (prn [ma mb ea eb])
   (let [xa (bit-and ma (bit-xor ma mb))
         xb (bit-and mb (bit-xor xa mb))
-        edge (fn [a b i j] ; Inline function definition of `edge`
-               (let [cp (- (* (ea i) (eb j)) (* (ea j) (eb i)))]
-                 (or (and (pos? cp) (pos? (bit-or xa a)) (pos? (bit-or xb b)))
-                     (and (neg? cp) (pos? (bit-or xa b)) (pos? (bit-or xb a))))))]
+        ; edge (fn [a b i j] ; Inline function definition of `edge`
+        ;        (let [cp (- (* (ea i) (eb j)) (* (ea j) (eb i)))]
+        ;          (or (and (pos? cp) (pos? (bit-or xa a)) (pos? (bit-or xb b)))
+        ;              (and (neg? cp) (pos? (bit-or xa b)) (pos? (bit-or xb a))))))]
+        edge (fn [a b i j]
+               (let [cp (- (* (ea i) (eb j)) (* (ea j) (eb i)))
+                     result (or (and (pos? cp) (pos? (bit-or xa a)) (pos? (bit-or xb b)))
+                                (and (neg? cp) (pos? (bit-or xa b)) (pos? (bit-or xb a))))]
+                 (println {
+                      :ea ea
+                      :eb eb
+                       :e-ai (ea i)
+                       :e-bj (eb j)
+                       :e-aj (ea i)
+                       :e-bi (eb j)
+                       ; :index-a a
+                       ; :index-b b
+                       :index-i i
+                       :index-j j
+                       ; :d0 (* (ea i) (eb j))
+                       ; :d1 (* (ea j) (eb i))
+                       ; :edge-mask-b b
+                      
+                       :cp cp
+                       :xa xa
+                       :xb xb
+                       :result result}
+                      ) (prn)
+                 result))]
     (not
      (or
       ; If 0b1111 == ma|mb, return early. If !=, not all faces are involved in test and
       ; there can't be an edge-based separation?
-      (not= 15 (bit-or ma mb))
+      ; (not= 15 (bit-or ma mb))
       ; Unique pairs of tetrahedron-edge bitmasks
       (edge 1 2 1 0)
       (edge 1 4 2 0)
@@ -138,12 +164,21 @@
                         (face-a #(dot % n) deltas)       ; :f
                         (face-a #(subdot % p n) verts))] ; :f*
             (if (< m 15)
-              (recur (conj masks m) (conj affine a) edges (next s))))
+              ; append m to masks, a to affine, then rerun loop with the new calues and next s
+              (recur (conj masks m) (conj affine a) edges (next s))
+            )
+          )
           ; f = :e
-          (if-not (edge-a (masks a) (masks b) (affine a) (affine b))
+          (if-not (edge-a (masks a) (masks b) (affine a) (affine b)); if not (edge-a?)
             ; Enter the loop again, incrementing through array s
-            (recur masks affine edges (next s)))))
-      masks)))
+            (recur masks affine edges (next s))
+          )
+        )
+      )
+      masks
+      )
+    )
+  )
 
 (defn check-faces-b?
   "Much like check-faces-a, but for 2nd tetra and specs encoding calls to face-b1/2?.
@@ -175,7 +210,7 @@
                [[:f 0 1] [:f 2 0] [:e 0 1] [:f 1 2] ; specs 
                 [:e 0 2] [:e 1 2] [:f* 4 3] [:e 0 3]
                 [:e 1 3] [:e 2 3]])]
-    (if masks ; exit early if masks is all nil, or all masks are 1111
+    (if masks ; exit early if masks is all nil, or all masks are 0b1111
       (or (not= 15 (apply bit-or masks)) ; bitwise or on array of masks, skipping if 
           (check-faces-b?
            (map #(sub % qa) p)
@@ -191,7 +226,7 @@
 
 (assert :first)
 
-(def q [[100.001 0.0 0.0] [150.0 50.0 0.0] [200.0 0.0 0.0] [150.0 25.0 10.0]])
-;; should = nil (false)
-(prn :second (intersect-tetrahedra? p q))
-(assert :second)
+; (def q [[100.001 0.0 0.0] [150.0 50.0 0.0] [200.0 0.0 0.0] [150.0 25.0 10.0]])
+; ;; should = nil (false)
+; (prn :second (intersect-tetrahedra? p q))
+; (assert :second)
