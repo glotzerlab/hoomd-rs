9e8b0f4ef9afb9d3cbce4f3bf080b7c296f13749
Joshua A. Anderson <joaander@umich.edu>
2025-05-29 15:51:27 -0400

    Implement Sphero

--- hoomd-geometry/src/shape/sphero.rs
+++ hoomd-geometry/src/shape/sphero.rs
@@ -3,11 +3,48 @@
 
 /*! Implement [`Sphero`] */
 
-/** Round a `Shape` with some radius.
+use crate::{IntersectsAt, SupportMapping, xenocollide::{collide3d, collide2d}};
+use hoomd_vector::{Angle, Cartesian, Vector, Versor};
+
+/** Round a shape with a given radius.
+
+[`Sphero`] modifies a given shape by sweeping it with a hypersphere of the given
+radius. The resulting [`Sphero<S>`] type is a shape itself. If `S` implements
+[`SupportMapping`], then [`Sphero<S>`] can be used in [`IntersectsAt`] tests with
+other convex shapes. See the full list of implementations below to see what other
+traits [`Sphero<S>`] implements for a given `S`.
+
+# Example
+
+Test if a circle overlaps with a rounded rectangle:
+```
+use hoomd_geometry::{Convex, IntersectsAt, shape::{Circle, Rectangle, Sphero}};
+use hoomd_vector::{Cartesian, Angle};
+use std::f64::consts::PI;
+
+let circle = Convex(Circle { r: 0.5 });
+let rectangle = Rectangle { edge_lengths: [3.0, 2.0].into() };
+let rounded_rectangle = Convex(Sphero { shape: rectangle, rounding_radius: 0.5 });
+
+assert!(rounded_rectangle.intersects_at(&circle, &[2.4, 0.0].into(), &Angle::default()));
+assert!(!rounded_rectangle.intersects_at(&circle, &[0.0, 2.4].into(), &Angle::default()));
+assert!(circle.intersects_at(&rounded_rectangle, &[0.0, 2.4].into(), &Angle::from(PI/2.0)));
 */
 pub struct Sphero<S> {
-    /// The struct to be rounded. This is typically a `Shape`, but does not have to be
+    /// The shape round.
     pub shape: S,
-    /// The radius of the rounding sphere
+    /// The radius of the rounding hypersphere.
     pub rounding_radius: f64,
 }
+
+impl<S, V> SupportMapping<V> for Sphero<S>
+where
+    S: SupportMapping<V>,
+    V: Vector,
+     {
+    #[inline]
+    fn support_mapping(&self, n: &V) -> V {
+        self.shape.support_mapping(n) + *n / n.norm() * self.rounding_radius
+    }
+}
+
