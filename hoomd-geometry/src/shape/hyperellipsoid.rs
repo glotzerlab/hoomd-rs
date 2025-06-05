f89a9008c9dc8cdf553b63a2e6db92838ddff028
janbridley <bradleyjenj@gmail.com>
2025-06-04 23:39:11 -0400

    Remove unused function

--- hoomd-geometry/src/shape/hyperellipsoid.rs
+++ hoomd-geometry/src/shape/hyperellipsoid.rs
@@ -214,68 +214,6 @@ impl Hyperellipsoid<3> {
         dead_code,
         reason = "Ported from HOOMD-Blue, with variable names maintained for consistency."
     )]
-    fn compute_ellipsoid_matrix<R>(&self, r_ij: &Cartesian<3>, o_ij: &R) -> Cartesian<10>
-    where
-        RotationMatrix<3>: From<R>,
-        R: Copy,
-    {
-        // See the HOOMD-Blue ShapeEllipsoid.h for the original source code.
-        let r = RotationMatrix::from(*o_ij);
-        let a = 1.0 / self.axes[0].powi(2);
-        let b = 1.0 / self.axes[1].powi(2);
-        let c = 1.0 / self.axes[2].powi(2);
-
-        let mut m = Cartesian::default();
-
-        // ...rotation part
-        // M[i][j] = a * R[i][0] * R[j][0] + b * R[i][1] * R[j][1] + c * R[i][2] * R[j][2];
-        m[0] = a * r.rows()[0][0] * r.rows()[0][0]
-            + b * r.rows()[0][1] * r.rows()[0][1]
-            + c * r.rows()[0][2] * r.rows()[0][2];
-        m[1] = a * r.rows()[1][0] * r.rows()[0][0]
-            + b * r.rows()[1][1] * r.rows()[0][1]
-            + c * r.rows()[1][2] * r.rows()[0][2];
-        m[2] = a * r.rows()[1][0] * r.rows()[1][0]
-            + b * r.rows()[1][1] * r.rows()[1][1]
-            + c * r.rows()[1][2] * r.rows()[1][2];
-        m[3] = a * r.rows()[2][0] * r.rows()[0][0]
-            + b * r.rows()[2][1] * r.rows()[0][1]
-            + c * r.rows()[2][2] * r.rows()[0][2];
-        m[4] = a * r.rows()[2][0] * r.rows()[1][0]
-            + b * r.rows()[2][1] * r.rows()[1][1]
-            + c * r.rows()[2][2] * r.rows()[1][2];
-        m[5] = a * r.rows()[2][0] * r.rows()[2][0]
-            + b * r.rows()[2][1] * r.rows()[2][1]
-            + c * r.rows()[2][2] * r.rows()[2][2];
-
-        // calculateTranslationPart(x, m);
-        // precalculation
-        let m0x0 = m[0] * r_ij[0];
-        let m1x0 = m[1] * r_ij[0];
-        let m1x1 = m[1] * r_ij[1];
-        let m2x1 = m[2] * r_ij[1];
-        let m3x0 = m[3] * r_ij[0];
-        let m3x2 = m[3] * r_ij[2];
-        let m4x1 = m[4] * r_ij[1];
-        let m4x2 = m[4] * r_ij[2];
-        let m5x2 = m[5] * r_ij[2];
-
-        // ...translation part
-        // m[i][3] = m[3][i] = -m[i][0] * x[0] - m[i][1] * x[1] - m[i][2] * x[2];
-        m[6] = -m0x0 - m1x1 - m3x2;
-        m[7] = -m1x0 - m2x1 - m4x2;
-        m[8] = -m3x0 - m4x1 - m5x2;
-        // ...mixed part
-        // m[3][3] = -1.0 + m[0][0] * x[0] * x[0] + m[1][1] * x[1] * x[1] + m[2][2] * x[2] * x[2] +
-        //           2.0 * (m[0][1] * x[0] * x[1] + m[1][2] * x[1] * x[2] + m[2][0] * x[2] * x[0]);
-        m[9] = -1.0
-            + r_ij[0] * (m0x0 + 2.0 * m1x1)
-            + r_ij[1] * (m2x1 + 2.0 * m4x2)
-            + r_ij[2] * (m5x2 + 2.0 * m3x0);
-
-        m
-    }
-}
 
 impl<const N: usize> BoundingSphereRadius for Hyperellipsoid<N> {
     #[inline]
