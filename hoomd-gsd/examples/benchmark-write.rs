49a3f77be6223d984869a6e81371ac2509cded92
Joshua A. Anderson <joaander@umich.edu>
2025-06-19 08:14:22 -0400

    Add command line options.

--- hoomd-gsd/examples/benchmark-write.rs
+++ hoomd-gsd/examples/benchmark-write.rs
@@ -5,12 +5,32 @@
 
 use hoomd_gsd::file_layer::GsdFile;
 use std::time::Instant;
+use clap::Parser;
+
+/// Arguments
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Number of keys to write per frame
+    #[arg(short, long, default_value_t=2)]
+    n_keys: usize,
+
+    /// f64 values per key
+    #[arg(short, long, default_value_t = 2048)]
+    key_size: usize,
+
+    /// File size to write (in MB)
+    #[arg(short, long, default_value_t = 256)]
+    file_size: usize,
+
+    /// Initial buffer size (in bytes)
+    #[arg(long, default_value_t = 1024)]
+    initial_buffer: usize,    
+}
 
 /// Measure the performance of writing a file.
-fn benchmark(buffer: usize) -> Result<f64, anyhow::Error> {
-    let n_keys: usize = 2;
-    let key_size: usize = 2048;
-    let target_file_size: usize = 256 * 1024 * 1024;
+fn benchmark(buffer: usize, n_keys: usize, key_size: usize, file_size_mb: usize) -> Result<f64, anyhow::Error> {
+    let target_file_size: usize = file_size_mb * 1024 * 1024;
 
     let n_frames = target_file_size / key_size / n_keys / size_of::<f64>();
 
@@ -42,12 +62,13 @@ fn benchmark(buffer: usize) -> Result<f64, anyhow::Error> {
     }
 
 fn main() -> Result<(), anyhow::Error> {
-    let mut buffer = 1024 * 1024;
+    let args = Args::parse();
+    let mut buffer = args.initial_buffer;
 
     println!("[");
     while buffer <= 64 * 1024 * 1024
         {
-        let mb_per_sec = benchmark(buffer)?;
+        let mb_per_sec = benchmark(buffer, args.n_keys, args.key_size, args.file_size)?;
         println!("[{buffer}, {mb_per_sec}],");
         buffer *= 2;
         }
