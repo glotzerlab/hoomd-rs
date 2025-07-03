ce9ea904dec02fe5711943fbca0d1a6810b0ee9d
Joshua A. Anderson <joaander@umich.edu>
2025-07-03 13:55:40 -0400

    Implement the fill example with bevy.

--- hoomd-mc/examples/fill-bevy.rs
+++ hoomd-mc/examples/fill-bevy.rs
@@ -12,38 +12,140 @@ use hoomd_mc::{Sweep, Translate, Trial};
 use hoomd_microstate::{Body, Microstate, MicrostateBuilder, boundary::Square, property::Point};
 use hoomd_vector::Cartesian;
 
-use bevy::prelude::*;
+use anyhow::Context;
+use bevy::{prelude::*, window::PresentMode};
+use std::time::{Duration, Instant};
+
+const FRAME_BUDGET: Duration = Duration::from_millis(14);
 
 fn main() -> anyhow::Result<()> {
     let mut app = App::new();
     app.add_plugins((
-        DefaultPlugins,
+        DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "fill".into(),
+                    present_mode: PresentMode::AutoVsync,
+                    ..default()
+                }),
+                ..default()
+            }),
     ))
     .insert_resource(ClearColor(Color::oklch(0.3, 0.0, 0.0)))
-    .add_systems(Startup, setup_scene);
+    .insert_resource(setup_simulation().context("failed to setup simulation")?)
+    .insert_resource(Time::<Fixed>::from_hz(10_000.0))
+    .add_systems(Startup, setup_scene)
+    .add_systems(Update, (step_simulation_system, sync_simulation).chain());
     
     app.run();
 
     Ok(())
 }
 
+/// The HOOMD simulation
+#[derive(Resource)]
+struct Simulation {
+    /// The simulation box vertical extent (in simulation units).
+    box_height: f64,
+    /// Positions of all the bodies in the simulation.
+    microstate: Microstate<Point<Cartesian<2>>, Point<Cartesian<2>>, Square>,
+    /// How sites interact with other sites and fields.
+    hamiltonian: (CutoffPair<Isotropic<Boxcar>>, Single<Linear<Cartesian<2>>>),
+    /// Trial moves to apply.
+    translate_sweep: Sweep<Translate>,
+    /// Temperature set point.
+    kt: f64
+}
+
+/// Set up the hoomd simulation
+fn setup_simulation() -> anyhow::Result<Simulation> {
+    let box_height = 10.0;
+    let kt = 1.0;
+    let d = 0.15;
+
+    let microstate = MicrostateBuilder::with_boundary(Square {
+        l: box_height.try_into()?,
+    })
+    .try_build()?;
+
+    let boxcar = Boxcar {
+        epsilon: 1000.0,
+        left: 0.0,
+        right: 1.0,
+    };
+    let evaluator = Isotropic(boxcar);
+    let cutoff_pair = CutoffPair {
+        r_cut: 1.0,
+        evaluator,
+    };
+
+    let linear = Single(Linear {
+        alpha: 10.0,
+        plane_origin: Cartesian::default(),
+        plane_normal: [0.0, 1.0].try_into()?,
+    });
+
+    let hamiltonian = (cutoff_pair, linear);
+
+    let translate = Translate {
+        maximum_distance: d.try_into()?,
+    };
+    let translate_sweep = Sweep { local: translate };
+
+    Ok(Simulation { box_height, microstate, hamiltonian, translate_sweep, kt })
+}
+
+/// Advance the simulation forward one step.
+fn step_simulation(simulation: &mut Simulation) -> anyhow::Result<()> {
+
+    if simulation.microstate.step() % 100 == 0 {
+        simulation.microstate.add_body(Body::point([0.0, simulation.box_height / 2.0 - 0.5].into()))?;
+    }
+
+    simulation.translate_sweep.apply(&mut simulation.microstate, &simulation.hamiltonian, &simulation.kt);
+    simulation.microstate.increment_step();
+    Ok(())
+    }
+
+/// Bevy system that advances the simulation forward one step.
+fn step_simulation_system(
+    mut exit: EventWriter<AppExit>,
+    simulation: ResMut<Simulation>) {
+
+    let simulation = simulation.into_inner();
+    let time = Instant::now();
+
+    while time.elapsed() < FRAME_BUDGET {
+        let result = step_simulation(simulation).with_context(|| format!("failed at step: {}", simulation.microstate.step()));
+        if let Err(error) = result {
+            error!("{error:?}");
+            exit.write(AppExit::Error(1.try_into().expect("1 is non-zero")));
+            break;
+            }
+        }
+    }
+    
 /// Assets that represent a Disk in the scene.
 #[derive(Resource)]
 struct Disk {
     /// The disk's mesh.
-    mesh: AssetId<Mesh>,
+    mesh: Handle<Mesh>,
     /// The disk's color.
-    color: AssetId<ColorMaterial>,
+    color: Handle<ColorMaterial>,
 }
+
+/// Mark entities as sites.
+#[derive(Component)]
+struct Site;
 
 /// Set up the bevy scene.
 fn setup_scene(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    simulation: Res<Simulation>,
 ) {
     let projection = Projection::Orthographic(OrthographicProjection {
-       scaling_mode: bevy::render::camera::ScalingMode::FixedVertical { viewport_height: 10.0 },
+       scaling_mode: bevy::render::camera::ScalingMode::FixedVertical { viewport_height: simulation.box_height as f32 },
        ..OrthographicProjection::default_2d()
     });
 
@@ -53,102 +155,35 @@ fn setup_scene(
 
     let mesh = meshes.add(Circle::new(0.5));
     let color = materials.add(Color::oklch(0.64, 0.14, 256.71));
-    commands.insert_resource(Disk { mesh: mesh.id(), color: color.id() });
-
-    commands.spawn((
-        Mesh2d(mesh),
-        MeshMaterial2d(color),
-        Transform::from_xyz(
-            0.0,
-            0.0,
-            0.0,
-        ),
-    ));    
+    commands.insert_resource(Disk { mesh, color });
 }
 
-// /// Run the simulation
-// fn run(mut terminal: DefaultTerminal) -> Result<(), Box<dyn std::error::Error>> {
-//     const FRAME_TIME: Duration = Duration::from_millis(10);
+/// Copy the current positions of simulation particles to bevy entities.
+fn sync_simulation(
+    mut commands: Commands,
+    disk: Res<Disk>,
+    simulation: Res<Simulation>,
+    mut query: Query<&mut Transform, With<Site>>) {
 
-//     let mut microstate = MicrostateBuilder::with_boundary(Square {
-//         l: 10.0.try_into()?,
-//     })
-//     .try_build()?;
+    let sites = simulation.microstate.sites();
+    let mut n_entities = 0;
+    
+    for (site_index, mut transform) in &mut query.into_iter().enumerate() {
+        let position = sites[site_index].properties.position;
+        transform.translation = Vec3 { x: position[0] as f32, y: position[1] as f32, z: 0.0 };
+        n_entities += 1;
+    }
 
-//     let boxcar = Boxcar {
-//         epsilon: 1000.0,
-//         left: 0.0,
-//         right: 1.0,
-//     };
-//     let evaluator = Isotropic(boxcar);
-//     let cutoff_pair = CutoffPair {
-//         r_cut: 1.0,
-//         evaluator,
-//     };
-
-//     let linear = Single(Linear {
-//         alpha: 10.0,
-//         plane_origin: Cartesian::default(),
-//         plane_normal: [0.0, 1.0].try_into()?,
-//     });
-
-//     let hamiltonian = (cutoff_pair, linear);
-
-//     let kt = 1.0;
-//     let d = 0.15;
-
-//     let translate = Translate {
-//         maximum_distance: d.try_into()?,
-//     };
-//     let translate_sweep = Sweep { local: translate };
-
-//     loop {
-//         let time = Instant::now();
-//         terminal.draw(|frame| render(frame, &microstate))?;
-
-//         if poll(Duration::from_millis(0))? && matches!(event::read()?, Event::Key(_)) {
-//             break Ok(());
-//         }
-
-//         if microstate.step() % 100 == 0 {
-//             microstate.add_body(Body::point([0.0, 4.5].into()))?;
-//         }
-
-//         translate_sweep.apply(&mut microstate, &hamiltonian, &kt);
-//         microstate.increment_step();
-
-//         let elapsed = time.elapsed();
-//         if elapsed < FRAME_TIME {
-//             sleep(FRAME_TIME - time.elapsed());
-//         }
-//     }
-// }
-
-// /// Render the system state.
-// fn render(
-//     frame: &mut Frame,
-//     microstate: &Microstate<Point<Cartesian<2>>, Point<Cartesian<2>>, Square>,
-// ) {
-//     let l = microstate.boundary().l.get();
-
-//     let canvas = Canvas::default()
-//         .block(Block::bordered().title(format!("Box fill (step: {})", microstate.step())))
-//         .marker(Marker::Braille)
-//         .paint(|ctx| {
-//             for site in microstate.sites() {
-//                 ctx.draw(&Circle {
-//                     x: site.properties.position[0],
-//                     y: site.properties.position[1],
-//                     radius: 0.5,
-//                     color: Color::Yellow,
-//                 });
-//             }
-//         })
-//         .x_bounds([-l / 2.0, l / 2.0])
-//         .y_bounds([-l / 2.0, l / 2.0]);
-
-//     let horizontal = Layout::horizontal([frame.area().height * 2]).flex(Flex::Center);
-//     let [area] = horizontal.areas(frame.area());
-
-//     frame.render_widget(canvas, area);
-// }
+    for site in &sites[n_entities..] {
+    commands.spawn((
+        Mesh2d(disk.mesh.clone()),
+        MeshMaterial2d(disk.color.clone()),
+        Transform::from_xyz(
+            site.properties.position[0] as f32,
+            site.properties.position[1] as f32,
+            0.0,
+        ),
+        Site,
+    ));    
+    }
+}
