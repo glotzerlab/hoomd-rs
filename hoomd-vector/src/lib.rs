833217101a2a15875003ca1310f1214f44f21868
Joshua A. Anderson <joaander@umich.edu>
2025-07-13 21:48:26 -0400

    Fix failing test.

--- hoomd-vector/src/lib.rs
+++ hoomd-vector/src/lib.rs
@@ -546,7 +546,7 @@ pub trait InnerProduct: Vector {
     where `self` is $`\vec{a}`$.
     # Example
     ```
-    use hoomd_vector::{Cartesian, Vector};
+    use hoomd_vector::{Cartesian, InnerProduct, Vector};
     let a = Cartesian::from([1.0, 2.0]);
     let b = Cartesian::from([4.0, 0.0]);
     let c = a.project(&b);
