b6fbf48075f501e5db5329d30b0513ef863f1328
mthran <mthran@umich.edu>
2025-07-18 16:08:31 -0400

    hyperbolic bevy

---
+++ examples/hyperbolic_fill.rs
@@ -0,0 +1,147 @@
+use hoomd_bevy::{
+    AdvanceSet, HoomdBevyPlugin, Settings, Simulation,
+    representation::{self, HyperbolicDiskAssets, HyperbolicDiskMaterial},
+};
+use rand::distr::Distribution;
+use hoomd_mc::{Sweep, Translate, Trial};
+use rand::{rngs::StdRng, SeedableRng};
+use hoomd_microstate::{Body, Microstate, MicrostateBuilder, boundary::Open, property::Point};
+use hoomd_manifold::{Minkowski, HyperbolicTranslate, Hyperboloid, CurvedIsotropic, HyperbolicDisk};
+use hoomd_vector::Cartesian;
+use hoomd_interaction::{
+    CutoffPair, pairwise::LennardJones};
+use libm::{cosh, sinh, acosh};
+use anyhow::Context;
+use bevy::prelude::*;
+
+/// Mark the disk representation type.
+struct A;
+const RHO: f64 = 1.0; 
+const PARTICLE_NUMBER : usize = 100;
+const RAD_SQ : f64 = 0.03;
+
+fn main() -> anyhow::Result<()> {
+    let simulation = Fill::new().context("failed to setup simulation")?;
+    let hoomd_bevy_plugin = HoomdBevyPlugin {
+        initial_settings: Settings {
+            viewport_height: 2.0 as f32,
+            ..default()
+        },
+        simulation,
+    };
+
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins);
+    hoomd_bevy_plugin.build(&mut app);
+    app.add_systems(Startup, (|| HyperbolicDiskMaterial::default()).pipe(representation::HyperbolicDisk::<A>::setup));
+    app.add_systems(
+        Update,
+        sync_simulation
+            .run_if(resource_changed::<Fill>)
+            .after(AdvanceSet),
+    );
+
+    app.run();
+
+    Ok(())
+}
+
+/// The HOOMD simulation
+#[derive(Resource)]
+struct Fill {
+    /// Positions of all the bodies in the simulation.
+    microstate: Microstate<Point<Minkowski<3>>, Point<Minkowski<3>>>,
+    /// How sites interact with other sites and fields.
+    hamiltonian: CutoffPair<CurvedIsotropic<LennardJones>>,
+    /// Trial moves to apply.
+    translate_sweep: Sweep<HyperbolicTranslate>,
+    /// Temperature set point.
+    kt: f64,
+}
+
+impl Fill {
+    /// Set up the hoomd simulation
+    fn new() -> anyhow::Result<Fill> {
+        let mut microstate = MicrostateBuilder::with_boundary(Open)
+    //.bodies([Body::point(Minkowski::from([1.0, -2.0, sqrt(5.0)])),
+    //    Body::point(Minkowski::from([1.0, -1.0, sqrt(3.0)])),
+    //    Body::point(Minkowski::from([-1.0, -2.0, sqrt(5.0)])),
+    //    Body::point(Minkowski::from([-1.0, -1.0, sqrt(3.0)]))])
+    .try_build()?;
+
+    let initial_spacing = 1.0;
+    let mut rng = StdRng::seed_from_u64(23);
+    let sample_disk = HyperbolicDisk{
+        r: initial_spacing.try_into()?, 
+        point: Minkowski::from([0.00001,0.00001,f64::sqrt(2.0*(0.00001_f64).powi(2) + RHO.powi(2))]),
+        skirt: RHO,}; 
+    for _n in 0..PARTICLE_NUMBER {
+        let new_point: Minkowski<3> = sample_disk.sample(&mut rng);
+        microstate.add_body(Body::point(new_point))?;
+    }
+    
+    let lj : LennardJones = LennardJones {
+        epsilon: 10.0,
+        sigma: 0.5,
+    };
+
+    let evaluator = CurvedIsotropic(lj, RHO);
+    let cutoff_pair = CutoffPair {
+        r_cut: 10.0,
+        evaluator,
+    };
+
+    let kt = 1.0;
+    let hamiltonian = cutoff_pair;
+    let d = 0.05;
+
+    let translate = HyperbolicTranslate {
+        maximum_distance: d.try_into()?,
+        skirt: RHO,
+    };
+    let translate_sweep = Sweep { local: translate };
+
+        Ok(Fill {
+            microstate,
+            hamiltonian,
+            translate_sweep,
+            kt,
+        })
+    }
+}
+
+impl Simulation for Fill {
+    /// Advance the simulation forward one step.
+    fn advance(&mut self) -> anyhow::Result<()> {
+        self.translate_sweep
+            .apply(&mut self.microstate, &self.hamiltonian, &self.kt);
+        self.microstate.increment_step();
+        Ok(())
+    }
+
+    /// Get the current simulation step.
+    fn step(&self) -> u64 {
+        self.microstate.step()
+    }
+}
+
+/// Copy the current positions of simulation particles to bevy entities.
+fn sync_simulation(
+    mut commands: Commands,
+    disk_assets: Res<HyperbolicDiskAssets<A>>,
+    query: Query<(Entity, &mut Transform), With<representation::HyperbolicDisk<A>>>,
+    simulation: Res<Fill>,
+) {
+    let sites = simulation.microstate.sites();
+    representation::HyperbolicDisk::sync(
+        &mut commands,
+        disk_assets,
+        query,
+        sites.iter().map(|site| {
+            (
+                site.properties.position,
+                RAD_SQ,
+            )
+        }),
+    );
+}
---
+++ hoomd-bevy/src/representation/hyperbolic_disk.rs
@@ -0,0 +1,178 @@
+// Copyright (c) 2024-2025 The Regents of the University of Michigan.
+// Part of hoomd-rs, released under the BSD 3-Clause License.
+
+/*! Implement Disk.
+*/
+
+use bevy::{
+    asset::embedded_asset,
+    prelude::*,
+    reflect::TypePath,
+    render::render_resource::{AsBindGroup, ShaderRef},
+    render::texture::TRANSPARENT_IMAGE_HANDLE,
+    sprite::{AlphaMode2d, Material2d, Material2dPlugin},
+};
+use itertools::EitherOrBoth::{Both, Left, Right};
+use itertools::Itertools;
+use std::marker::PhantomData;
+use hoomd_manifold::{Minkowski, Hyperboloid};
+use libm::{cosh, sinh, acosh};
+use crate::PRIMARY_COLOR;
+
+/// Location of the shader implementation
+const SHADER_ASSET_PATH: &str = "embedded://hoomd_bevy/representation/disk.wgsl";
+const RHO : f64 = 1.0;
+
+/** Represent an entity with a 2D disk in the xy plane.
+
+The base representation has a diameter of 1.0. Provide a non-unit diameter in
+[`sync`] to render disks of different sizes. Nominally, the z coordinate of the
+disks should be set to 0. Choose a different value to control the back to front
+draw order.
+
+All disks of the same type must have the same material. To display disks with
+different colors, outline widths, or textures, `setup` and `sync` multiple types
+of disks with different marker types.
+
+To use:
+* Add [`setup`](Self::setup) to the `Startup` schedule.
+* Call [`sync`](Self::sync) in an `Update` schedule that runs after `AdvanceSet`.
+*/
+#[derive(Component)]
+pub struct HyperbolicDisk<T> {
+    /// Mark the type of the disk.
+    marker: PhantomData<T>,
+}
+
+/// Assets that represent a Disk in the scene.
+#[derive(Resource)]
+pub struct HyperbolicDiskAssets<T> {
+    /// The disk mesh.
+    mesh: Handle<Mesh>,
+    /// The disk material.
+    material: Handle<HyperbolicDiskMaterial>,
+    /// Mark the type of the disk assets.
+    marker: PhantomData<T>,
+}
+
+/// Initialize needed plugins and add assets for this representation.
+pub(crate) fn build(app: &mut App) {
+    app.add_plugins(Material2dPlugin::<HyperbolicDiskMaterial>::default());
+    embedded_asset!(app, "hyperbolic_disk.wgsl");
+}
+
+impl<T: Send + Sync + 'static> HyperbolicDisk<T> {
+    /** Create assets to render disks.
+     */
+    pub fn setup(
+        material: In<HyperbolicDiskMaterial>,
+        mut commands: Commands,
+        mut meshes: ResMut<Assets<Mesh>>,
+        mut materials: ResMut<Assets<HyperbolicDiskMaterial>>,
+    ) {
+        let mesh = meshes.add(Rectangle::new(1.0, 1.0));
+        let material = materials.add(material.0);
+        commands.insert_resource(HyperbolicDiskAssets::<T> {
+            mesh,
+            material,
+            marker: PhantomData,
+        });
+    }
+
+    /// Copy the current positions of simulation particles to bevy entities.
+    pub fn sync<I>(
+        commands: &mut Commands,
+        disk_assets: Res<HyperbolicDiskAssets<T>>,
+        query: Query<(Entity, &mut Transform), With<Self>>,
+        disks: I,
+    ) where
+        I: IntoIterator<Item = (Minkowski<3>, f64)>,
+    {
+        for item in &mut query.into_iter().zip_longest(disks) {
+            match item {
+                Both((_, mut transform), (position, diameter)) => {
+                    let (poincare_position, projected_radius) = poincare(&position, RHO, diameter);
+                    transform.translation = Vec3::from_array(poincare_position);
+                    transform.scale = Vec3::splat(projected_radius);
+                }
+                Left((entity, _)) => commands.entity(entity).despawn(),
+                Right((position, diameter)) => {
+                    let (poincare_position, projected_radius) = poincare(&position, RHO, diameter);
+                    commands.spawn((
+                        Mesh2d(disk_assets.mesh.clone()),
+                        MeshMaterial2d(disk_assets.material.clone()),
+                        Transform::from_translation(Vec3::from_array(poincare_position)).with_scale(Vec3::splat(projected_radius)),
+                        Self {
+                            marker: PhantomData,
+                        },
+                    ));
+                }
+            }
+        }
+    }
+}
+
+/// Project coordinates to Poincare disk 
+///TODO: fix radius
+fn poincare(point: &Minkowski<3>, skirt: f64, rad_sq: f64) -> ([f32;3], f32) {
+    let proj = point.to_poincare(skirt);
+    let v = acosh((rad_sq + RHO.powi(2))/(RHO.powi(2)-rad_sq));
+    let eta = acosh(point.coordinates[2]/RHO);
+    let edge_proj = (RHO * sinh(eta+v))/(1.0 + cosh(eta+v));
+    let rad_proj = (RHO * sinh(eta))/(1.0 + cosh(eta)) - edge_proj;
+    ([proj[0] as f32, proj[1] as f32, 0.0_f32], rad_proj as f32)
+}
+
+/** Control how disks are rendered.
+
+[`DiskMaterial`] mixes the texture (which defaults to fully transparent) with
+the background color using the texture alpha. It ignores the background alpha.
+
+Control the draw order using the z coordinate. The draw order is non-deterministic
+for all disks at the same z value.
+*/
+#[derive(Asset, TypePath, AsBindGroup, Debug, Clone)]
+pub struct HyperbolicDiskMaterial {
+    /// Color applied to the interior of the disk.
+    #[uniform(0)]
+    pub background_color: LinearRgba,
+
+    /// Color applied to the outline.
+    #[uniform(1)]
+    pub outline_color: LinearRgba,
+
+    /// Width of the outline.
+    #[uniform(2)]
+    pub outline_width: f32,
+
+    /// Factor to scale the texture by.
+    #[uniform(3)]
+    pub texture_scale: f32,
+
+    /// Texture to apply. Blended with `color`.
+    #[texture(4)]
+    #[sampler(5)]
+    pub texture: Handle<Image>,
+}
+
+impl Default for HyperbolicDiskMaterial {
+    fn default() -> Self {
+        Self {
+            background_color: PRIMARY_COLOR.into(),
+            outline_color: Color::linear_rgb(0.0, 0.0, 0.0).into(),
+            outline_width: 0.05,
+            texture: TRANSPARENT_IMAGE_HANDLE,
+            texture_scale: 1.2,
+        }
+    }
+}
+
+impl Material2d for HyperbolicDiskMaterial {
+    fn fragment_shader() -> ShaderRef {
+        SHADER_ASSET_PATH.into()
+    }
+
+    fn alpha_mode(&self) -> AlphaMode2d {
+        AlphaMode2d::Mask(0.5)
+    }
+}
