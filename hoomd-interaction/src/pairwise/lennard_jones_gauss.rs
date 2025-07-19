fa37c445ac0634403fe8fe924e50b530a4fa3b75
Michelle Thran <mthran@umich.edu>
2025-07-19 14:01:28 -0400

    fixed unit issue, removed default, and added tests

--- hoomd-interaction/src/pairwise/lennard_jones_gauss.rs
+++ hoomd-interaction/src/pairwise/lennard_jones_gauss.rs
@@ -9,7 +9,7 @@ use super::{IsotropicEnergy, IsotropicForce};
 /** Double-well potential with a steep repulsive core
 
 ```math
-U(r) = \frac{1}{r^{12}} - \frac{2}{r^6} - \varepsilon \exp \left(-\frac{(r-r_0)^2}{2\sigma^2}\right)
+U(r) = 1[\mathrm{energy}]\cdot\left[ \left(\frac{1[\mathrm{length}]}{r}\right)^{12} - 2\left(\frac{1[\mathrm{length}]}{r}\right)^{6}\right] - \varepsilon \exp \left(-\frac{(r-r_0)^2}{2\sigma^2}\right)
 ```
 
 Compute the Lennard-Jones-Gauss (LJG) potential and force as a function of `r`.
@@ -33,40 +33,24 @@ The parameters are public fields and may be accessed directly:
 ```
 use hoomd_interaction::pairwise::{LennardJonesGauss};
 
-let mut lennard_jones_gauss: LennardJonesGauss = LennardJonesGauss::default();
+let mut lennard_jones_gauss: LennardJonesGauss = LennardJonesGauss{
+    epsilon: 1.5,
+    sigma_squared: 0.02,
+    r_0:  3.2
+};
 lennard_jones_gauss.epsilon = 1.5;
-lennard_jones_gauss.sigma_squared = 3.0;
+lennard_jones_gauss.sigma_squared = 0.02;
+lennard_jones_gauss.r_0 = 3.2;
 ```
 */
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct LennardJonesGauss {
-    /// Scale of Gaussian
+    /// Scale of Gaussian, in units of energy
     pub epsilon: f64,
-    /// Width of Gaussian
+    /// Width of Gaussian, sigma^2 is in units of length squared
     pub sigma_squared: f64,
-    /// Gaussian center
+    /// Gaussian center, in units of length
     pub r_0: f64,
-}
-
-impl Default for LennardJonesGauss {
-    /** Construct a [`LennardJonesGauss`] with default parameters (epsilon=1.0, `sigma_squared=1.0`, `r_0=0.0`)
-
-    # Example
-
-    ```
-    use hoomd_interaction::pairwise::LennardJonesGauss;
-
-    let lennard_jones_gauss: LennardJonesGauss = LennardJonesGauss::default();
-    ```
-    */
-    #[inline]
-    fn default() -> Self {
-        Self {
-            epsilon: 1.0,
-            sigma_squared: 1.0,
-            r_0: 0.0,
-        }
-    }
 }
 
 impl IsotropicEnergy for LennardJonesGauss {
@@ -84,6 +68,82 @@ impl IsotropicForce for LennardJonesGauss {
         let r_inv = r.recip();
         let arg = -1.0 * (r - self.r_0).powi(2) / (2.0 * self.sigma_squared);
         12.0 * (r_inv.powi(13) - r_inv.powi(7))
-            - (self.epsilon * (r - self.r_0) / self.sigma_squared) * arg.exp()
+            - (self.epsilon * (self.r_0 - r) / self.sigma_squared) * arg.exp()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ::approx::{assert_abs_diff_eq, assert_relative_eq};
+    use rstest::*;
+
+    #[rstest]
+    fn select_points_first_set() {
+        let lj_gauss: LennardJonesGauss = LennardJonesGauss {
+            epsilon: 2.0,
+            sigma_squared: 0.5,
+            r_0: 3.0,
+        };
+
+        assert_eq!(lj_gauss.epsilon, 2.0);
+        assert_eq!(lj_gauss.sigma_squared, 0.5);
+        assert_eq!(lj_gauss.r_0, 3.0);
+
+        // numeric tests
+        assert_relative_eq!(
+            lj_gauss.energy(1.5_f64),
+            -0.378_674_092_892_274,
+            epsilon = 1e-12
+        );
+        assert_abs_diff_eq!(
+            lj_gauss.force(1.5_f64),
+            -1.273_068_535_928_335,
+            epsilon = 1e-12
+        );
+        assert_relative_eq!(
+            lj_gauss.energy(3.2_f64),
+            -1.923_440_656_092_139,
+            epsilon = 1e-12
+        );
+        assert_abs_diff_eq!(
+            lj_gauss.force(3.2_f64),
+            0.765_142_344_273_568,
+            epsilon = 1e-12
+        );
+    }
+    #[rstest]
+    fn select_points_second_set() {
+        let lj_gauss: LennardJonesGauss = LennardJonesGauss {
+            epsilon: 10.0,
+            sigma_squared: 0.1,
+            r_0: 5.0,
+        };
+
+        assert_eq!(lj_gauss.epsilon, 10.0);
+        assert_eq!(lj_gauss.sigma_squared, 0.1);
+        assert_eq!(lj_gauss.r_0, 5.0);
+
+        // numeric tests
+        assert_relative_eq!(
+            lj_gauss.energy(1.5_f64),
+            -0.167_875_643_768_546,
+            epsilon = 1e-12
+        );
+        assert_abs_diff_eq!(
+            lj_gauss.force(1.5_f64),
+            -0.640_673_188_557_149,
+            epsilon = 1e-12
+        );
+        assert_relative_eq!(
+            lj_gauss.energy(3.2_f64),
+            -0.001_862_699_147_576_425,
+            epsilon = 1e-12
+        );
+        assert_abs_diff_eq!(
+            lj_gauss.force(3.2_f64),
+            -0.003_505_791_529_792_807,
+            epsilon = 1e-12
+        );
     }
 }
