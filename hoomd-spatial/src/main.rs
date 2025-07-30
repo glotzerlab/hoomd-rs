92c00d6d77ce490c547c21ccce1d89d38a8b21fd
Domagoj Fijan <domagoj.fijan@gmail.com>
2025-07-30 11:39:49 -0400

    remove file that's not needed

--- hoomd-spatial/src/main.rs
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2024-2025 The Regents of the University of Michigan.
-// Part of hoomd-rs, released under the BSD 3-Clause License.
-
-fn main() {
-    println!("Hello, world!");
-}
