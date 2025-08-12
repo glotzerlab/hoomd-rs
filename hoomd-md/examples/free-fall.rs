633759de9224ecc943d0997994f028c992c7d06c
josephburkhart <joaburkh@umich.edu>
2025-08-12 10:45:36 -0400

    Intermediate changes to md free fall example

--- hoomd-md/examples/free-fall.rs
+++ hoomd-md/examples/free-fall.rs
@@ -3,25 +3,32 @@
 /*! This is an example
 */
 
+use hoomd_interaction;
 use hoomd_microstate::property::Position;
 use hoomd_microstate::{Body, Microstate};
 use hoomd_vector::Cartesian;
-use hoomd_md::{thermostat, ConstantVolume};
-use hoomd_md::thermostat::NoThermostat;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut microstate = Microstate::new();
     microstate.add_body(Body::point(Cartesian::from([0.0, 0.0])))?;
 
-    // What's needed for an integrator?
-    let dt = 0.01;
-    let kT = 1.0;
-    let thermostat = NoThermostat;
+    let kt = 1.0;
+    let hamiltonian = hoomd_interaction::Single(
+        hoomd_interaction::external::Linear {
+            alpha: 2.0,
+            plane_origin: [0.0, 0.0].into(),
+            plane_normal: [0.0, 1.0].try_into()?,
+        }
+    );
+    let d = 0.1;
 
-    // let integrator = ConstantVolume {};
+    let translate = hoomd_mc::Translate {
+        maximum_distance: d.try_into()?,
+    };
+    let translate_sweep = hoomd_mc::Sweep { local: translate };
 
-    for _ in 0..10 {
-        // integrator.integrate(&mut microstate);
+    for _ in 0..100_000 {
+        translate_sweep.apply(&mut microstate, &hamiltonian, &kt);
         println!("{}", microstate.bodies()[0].item.properties.position());
         microstate.increment_step();
     }
