c03b55f20cc3a398e11ca0e43ec9055dc9285351
Joseph Burkhart <61951318+josephburkhart@users.noreply.github.com>
2025-08-20 14:39:06 -0400

    Implement verlet integration for translation under constant volume

--- hoomd-md/src/lib.rs
+++ hoomd-md/src/lib.rs
@@ -14,14 +14,18 @@ TODO: Expand documentation.
  */
 
 pub mod thermostat;
+use std::ops::AddAssign;
+
+use hoomd_interaction::NetBodyForce;
+use hoomd_vector::{Vector, Cartesian};
 use thermostat::{Thermostat, NoThermostat};
-use hoomd_microstate::Microstate;
+use hoomd_microstate::{boundary::Boundary, property::{Acceleration, Mass, Position, Velocity}, Microstate, Transform};
 
 /** Integrate over translational degrees of freedom. 
  */
-pub trait TranslationalMotion<B, S, C> {
+pub trait TranslationalMotion<B, S, C, F> {
     /// Integrate over translational degrees of freedom. 
-    fn integrate_translation(&self, microstate: &mut Microstate<B, S, C>);
+    fn integrate_translation(&self, microstate: &mut Microstate<B, S, C>, force: &F);
 }
 
 /** Integrate over rotational degrees of freedom. 
@@ -37,7 +41,7 @@ pub struct ConstantVolume<T: Thermostat> {
     pub dt: f64,
 
     /// The temperature in units of kT.
-    pub kt: f64,
+    pub kT: f64,
 
     /// The thermostat.
     pub thermostat: T,
@@ -46,17 +50,59 @@ pub struct ConstantVolume<T: Thermostat> {
 /// TODO: add documentation
 pub struct ConstantPressure;
 
-
-impl<B, S, C, T: Thermostat> TranslationalMotion<B, S, C> for ConstantVolume<T>
+/// TODO: add documentation
+impl<V, B, S, C, T, F> TranslationalMotion<B, S, C, F> for ConstantVolume<T>
 where 
-    T: Thermostat
+    // B: Position<Vector = Cartesian<N>>
+    V: Default + Vector,
+    B: Position<Vector = V> + Velocity<Vector = V> + Acceleration<Vector = V> + Mass + Transform<S> + Clone,
+    S: Position<Vector = V>,
+    C: Boundary<V, B, S>,
+    T: Thermostat,
+    F: NetBodyForce<V, B, S, C>
 {
+    // TODO: Do we need to allow users to set a displacement limit?
     #[inline]
-    fn integrate_translation(&self, microstate: &mut Microstate<B, S, C>) {
+    fn integrate_translation(
+        &self,
+        microstate: &mut Microstate<B, S, C>,
+        force: &F,
+    ) {
+        let rescaling_factor = self.thermostat.temperature_factor();
+
+        // Integration Step One
         // For loop over a range instead of bodies().iter() since the latter holds an immutable borrow.
-
         for body_index in 0..microstate.bodies().len() {
+            // Get the important information from the body
+            let mut body_properties = microstate.bodies()[body_index].item.properties.clone();
 
+            // Perform the integration step
+            let acceleration = *body_properties.acceleration();
+            *body_properties.velocity_mut() += (acceleration * 0.5 * self.dt) 
+                * rescaling_factor;
+            *body_properties.position_mut() = *body_properties.velocity() * self.dt;
+
+            // Update body properties accordingly, wrapping automatically
+            microstate.update_body_properties(body_index, body_properties)
+                .expect("Bodies and sites should remain in simulation boundary.");
+        }
+
+        // Integration Step Two
+        for body_index in 0..microstate.bodies().len() {
+            // Get the important information from the body
+            let mut body_properties = microstate.bodies()[body_index].item.properties.clone();
+        
+            // Calculate the net force on the body
+            let net_force = force.net_force_on_body(microstate, body_index);    // should bundle both pairwise and non-pairwise
+            
+            // Perform the integration step
+            *body_properties.acceleration_mut() = net_force / *body_properties.mass();
+            let acceleration = *body_properties.acceleration();
+            *body_properties.velocity_mut() += (acceleration * 0.5 * self.dt) * rescaling_factor;
+
+            // Update body properties accordingly
+            microstate.update_body_properties(body_index, body_properties)
+                .expect("Bodies and sites should remain in simulation boundary.");
         }
 
         microstate.increment_substep();
