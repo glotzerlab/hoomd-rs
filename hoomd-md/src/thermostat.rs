2f9f428580f6d3bad704688e37b3cf44f464689c
Joseph Burkhart <61951318+josephburkhart@users.noreply.github.com>
2025-09-11 13:27:45 -0400

    Change thermostat to use the new macrostate API

--- hoomd-md/src/thermostat.rs
+++ hoomd-md/src/thermostat.rs
@@ -1,20 +1,46 @@
 // Copyright (c) 2024-2025 The Regents of the University of Michigan.
 // Part of hoomd-rs, released under the BSD 3-Clause License.
 
-use rand_distr::{Distribution, Gamma, Normal};
-use hoomd_vector::{Vector, Cartesian};
-use hoomd_microstate::{boundary::{GenerateGhosts, Wrap}, property::{Acceleration, Mass, Position, Velocity}, Microstate, Transform};
 /*! Control system temperature.
 
 TODO: Expand documentation.
  */
 
+use rand::Rng;
+use rand_distr::{Distribution, Gamma, Normal};
+use hoomd_vector::{Vector, Cartesian};
+use hoomd_microstate::{boundary::{GenerateGhosts, Wrap}, property::{Acceleration, Mass, Position, Velocity}, Microstate, Transform};
+use hoomd_simulation::macrostate::{Isoentropic, Isothermal};
+
 /** Adjust the temperature of a system.
+
+TODO: ensure that docs indicate two-step integration is baked into the thermostat trait
 TODO: Add example.
 */
- pub trait Thermostat {
-    /// The scaling factor for velocities.
-    fn temperature_factor(&self) -> f64;
+ pub trait Thermostat<M, B, S, C> {
+    /// The scaling factor for velocities in the first half-step.
+    /// Note that translation and rotation are assumed to have identical math
+    /// behind their scaling factors.
+    fn rescaling_factor_step_one(
+        &self,
+        macrostate: &M,
+        microstate: &Microstate<B, S, C>,
+        dt: f64,
+        degrees_of_freedom: u32,
+        kinetic_energy: f64,
+    ) -> f64;
+
+    /// The scaling factor for velocities in the second half-step.
+    /// Note that translation and rotation are assumed to have identical math
+    /// behind their scaling factors.
+    fn rescaling_factor_step_two(
+        &self,
+        macrostate: &M,
+        microstate: &Microstate<B, S, C>,
+        dt: f64,
+        degrees_of_freedom: u32,
+        kinetic_energy: f64,
+    ) -> f64;
 }
 
 /** Constant temperature.
@@ -22,9 +48,31 @@ TODO: Add example.
 */
 pub struct NoThermostat;
 
-impl Thermostat for NoThermostat {
+impl<M, B, S, C> Thermostat<M, B, S, C> for NoThermostat
+where
+    M: Isoentropic
+{
     #[inline]
-    fn temperature_factor(&self) -> f64 {
+    fn rescaling_factor_step_one(
+        &self,
+        macrostate: &M,
+        microstate: &Microstate<B, S, C>,
+        dt: f64,
+        degrees_of_freedom: u32,
+        kinetic_energy: f64,
+    ) -> f64 {
+        1.0
+    }
+
+    #[inline]
+    fn rescaling_factor_step_two(
+        &self,
+        macrostate: &M,
+        microstate: &Microstate<B, S, C>,
+        dt: f64,
+        degrees_of_freedom: u32,
+        kinetic_energy: f64,
+    ) -> f64 {
         1.0
     }
 }
@@ -34,39 +82,39 @@ TODO: Add documentation.
 TODO: Add example.
 */
 pub struct  BussiThermostat {
-    /// Temperature set point for the thermostat (`[energy]`).
-    pub kt: f64,
-
+    
     /// Thermostat time constant (`[time]`).
     pub tau: f64,
 }
 
 /// TODO: add documentation
-impl<V, B, S, C> Thermostat for BussiThermostat
+impl<M, B, S, C> Thermostat<M, B, S, C> for BussiThermostat
 where
-    V: Default + Vector,
-    B: Position<Vector = V> + Velocity<Vector = V> + Acceleration<Vector = V> + Mass + Transform<S> + Clone,
-    S: Position<Vector = V> + Default,
-    C: Wrap<B> + Wrap<S> + GenerateGhosts<S>,
+    M: Isothermal
 {
     /** Calculate velocity rescaling factor following the Appendix in https://doi.org/10.1063/1.2408420.
         Bussi requires the rng, instataneous kinetic_energy, timestep and degrees-of-freedom,
         change the trait function definition accordingly?
     */
     #[inline]
-    fn temperature_factor(
+    fn rescaling_factor_step_one(
         &self,
-        microstate: &mut Microstate<B, S, C>,
+        macrostate: &M,
+        microstate: &Microstate<B, S, C>,
         kinetic_energy: &f64,
         dt: &f64,
-        dof: &i64) -> f64 {
+        dof: &i64
+    ) -> f64 {
+        // panic if there is no kT setpoint
+        let kT = macrostate.temperature();
+
         // panic if momenta was not initialized
         assert!(!(*kinetic_energy == 0.0 && *dof != 0), "Bussi thermostat requires non-zero initial momenta.");
 
         // get rng from microstate to ensure reproducibility
         let mut rng = microstate.counter().make_rng();
 
-        // trivial case when no particles present.
+        // trivial case when no particles are present
         if *dof == 0 {
             return 1.0
         }
@@ -86,7 +134,7 @@ where
             random_gamma = 2.0 * Gamma::new((*dof as f64 - 1.0) / 2.0, 1.0).unwrap().sample(&mut rng);
         }
         // assemble everything
-        let v = self.kt / 2.0 / kinetic_energy;
+        let v = self.kT / 2.0 / kinetic_energy;
         let term1 = v * (1.0 - time_decay_factor) * (random_gamma + random_normal_one.powi(2));
         let term2 = 2.0 * random_normal_one * (v * (1.0 - time_decay_factor) * time_decay_factor).sqrt();
         (time_decay_factor + term1 + term2).sqrt()
