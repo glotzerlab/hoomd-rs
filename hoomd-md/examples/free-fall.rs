45e815797f8341c4cf05980bce3558826518248d
Alex <shihkual@umich.edu>
2025-09-16 13:08:00 -0400

    Modify free-fall example

--- hoomd-md/examples/free-fall.rs
+++ hoomd-md/examples/free-fall.rs
@@ -4,14 +4,16 @@
 /*! Simple example of a falling body with MD.
 */
 
+use hoomd_simulation::macrostate::{Isoentropic, Isochoric};
 use hoomd_vector::Cartesian;
 use hoomd_microstate::{Microstate, Body, property::{Point, DynamicsPoint, Position}};
-use hoomd_interaction::{Single, external::Linear};
+use hoomd_interaction::{external::Linear, pairwise::Isotropic, Single};
 use hoomd_md::{ConstantVolume, TranslationalMotion, thermostat::NoThermostat};
 
 // Question: Why do I have to import TranslationalMotion and Position?
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Initialize a particle.
     let mut microstate = Microstate::new();
     let body = Body {
         properties: DynamicsPoint {
@@ -25,24 +27,43 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     microstate.add_body(body)?;
 
+    // Define my set macrostat.
+    struct IsoentropicMacrostat {}
+    impl Isoentropic for IsoentropicMacrostat {}
+    impl Isochoric for IsoentropicMacrostat{}
+
+    let macrostate = IsoentropicMacrostat{};
+
+    // Add gravity
     let force = Single(Linear {
-        alpha: 2.0,
+        alpha: -2.0,
         plane_origin: [0.0, 1.0].into(),
         plane_normal: [0.0, 1.0].try_into()?,
     });
 
     let (kT, dt) = (1.0, 0.1);
-    let thermostat = NoThermostat;
+    let mut thermostat = NoThermostat;
 
-    let integrator = ConstantVolume { dt, kT, thermostat };
+    let mut integrator = ConstantVolume::new(dt);
 
-    for _ in 0..10 {
-        integrator.integrate_translation(
+    for timestep in 0..10 {
+        integrator.integrate_translation_step_one(
             &mut microstate,
-            &force
+            &force,
+            &mut thermostat,
+            &macrostate,
         );
 
+        integrator.integrate_translation_step_two(
+            &mut microstate,
+            &force,
+            &mut thermostat,
+            &macrostate,
+        );
+        
+        println!("==============={}===============", timestep);
         println!("Position of body 0: {}", microstate.bodies()[0].item.properties.position);
+        println!("Kinetic energy of body 0: {}", integrator.get_kinetic_energy());
         microstate.increment_step();
     }
 
