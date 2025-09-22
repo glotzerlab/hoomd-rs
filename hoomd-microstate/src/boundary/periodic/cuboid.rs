e7f246aeba2c92ef665ee9781e435e3fdeeb844e
Joshua A. Anderson <joaander@umich.edu>
2025-09-22 15:41:06 -0400

    Fix clippy warnings.

--- hoomd-microstate/src/boundary/periodic/cuboid.rs
+++ hoomd-microstate/src/boundary/periodic/cuboid.rs
@@ -524,10 +524,10 @@ mod tests {
             let periodic = Periodic::new(0.0, cuboid).expect("hard-coded range should be valid");
 
             let point = Point::new([5.0, 3.0, 8.0].into());
-            assert_eq!(periodic.wrap(point.clone()), Ok(point));
+            assert_eq!(periodic.wrap(point), Ok(point));
 
             let point = Point::new([-10.0, -10.0, -10.0].into());
-            assert_eq!(periodic.wrap(point.clone()), Ok(point));
+            assert_eq!(periodic.wrap(point), Ok(point));
 
             let point = Point::new(
                 [
@@ -537,7 +537,7 @@ mod tests {
                 ]
                 .into(),
             );
-            assert_eq!(periodic.wrap(point.clone()), Ok(point));
+            assert_eq!(periodic.wrap(point), Ok(point));
 
             let point = Point::new([10.0, 10.0, 10.0].into());
             assert_eq!(
