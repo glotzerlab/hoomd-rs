0bbbf6b7f2481bebe515bc598484ade37bf24d32
janbridley <bradleyjenj@gmail.com>
2025-11-05 11:03:59 -0500

    Docs in backends

--- threefry/src/backends.rs
+++ threefry/src/backends.rs
@@ -18,13 +18,13 @@ pub(crate) fn rotl_u64(x: u64, d: u32) -> u64 {
 pub(crate) fn rotl_u32(x: u32, d: u32) -> u32 {
     (x << d) | (x >> (32 - d))
 }
-/// TODO.
+/// Mixing function for the ThreeFry2x64 PRNG.
 #[inline]
 pub(crate) fn mix2x64(state: &mut [u64; 2], round_key: u32) {
     state[0] = state[0].wrapping_add(state[1]);
     state[1] = rotl_u64(state[1], round_key) ^ state[0];
 }
-/// .
+/// Mixing function for the ThreeFry4x64 PRNG.
 #[inline]
 pub(crate) fn mix4x64(state: &mut [u64; 4], rotations: (u32, u32), round: usize) {
     if round.is_multiple_of(2) {
@@ -39,7 +39,12 @@ pub(crate) fn mix4x64(state: &mut [u64; 4], rotations: (u32, u32), round: usize)
         state[1] = rotl_u64(state[1], rotations.1) ^ state[2];
     }
 }
-/// TODO: unsafe if N < slice size
+/// Read a little-endian u64 from a byte array.
+///
+/// # Panics
+///
+/// This function will panic if the provided range is not 8 bytes long or is
+/// out of bounds for the array.
 #[inline]
 pub(crate) fn read_u64_le_unchecked<const N: usize>(
     stream: [u8; N],
@@ -47,7 +52,12 @@ pub(crate) fn read_u64_le_unchecked<const N: usize>(
 ) -> u64 {
     u64::from_le_bytes(stream[range].try_into().unwrap_or_else(|_| unreachable!()))
 }
-/// TODO: unsafe if N < slice size
+/// Read a little-endian u32 from a byte array.
+///
+/// # Panics
+///
+/// This function will panic if the provided range is not 4 bytes long or is
+/// out of bounds for the array.
 #[inline]
 pub(crate) fn read_u32_le_unchecked<const N: usize>(
     stream: [u8; N],
