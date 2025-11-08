03970e46e98e25c4d6f7822a35f6ed0419cbb555
Joshua A. Anderson <joaander@umich.edu>
2025-11-07 20:23:16 -0500

    Remove unused dependencies and fix failing bench build.

--- hoomd-vector/benches/cartesian.rs
+++ hoomd-vector/benches/cartesian.rs
@@ -8,7 +8,8 @@
 
 //! Benchmark Cartesian
 use divan::{self, Bencher, black_box, counter::ItemsCount};
-use hoomd_utility::{random::Counter, valid::PositiveReal};
+use hoomd_utility::valid::PositiveReal;
+use hoomd_rand::Counter;
 use hoomd_vector::{Cartesian, Cross, InnerProduct, distribution::Ball};
 use rand::{
     Rng, SeedableRng,
