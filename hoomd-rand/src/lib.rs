9937fb6def307f3fa55496c7f25f17980ae08a8e
Joshua A. Anderson <joaander@umich.edu>
2025-11-10 10:01:32 -0500

    Clarify SFC64 default.

--- hoomd-rand/src/lib.rs
+++ hoomd-rand/src/lib.rs
@@ -5,8 +5,8 @@
 //!
 //! This crate offers implementations of several modern RNG algorithms, including:
 //!
-//! - [`SFC64`]: The "Small Fast Chaotic" counter based RNG, which should be considered
-//!   the default in most cases. It is extremely fast, has low latency, and is very
+//! - [`SFC64`]: The "Small Fast Chaotic" counter based RNG, which should used by default
+//!   in most cases. It is extremely fast, has low latency, and is very
 //!   statistically sound -- we have validated that streams are independent and
 //!   uncorrelated for >2TB of data per seed.
 //! - [`AESRand`]: An AES-based RNG (currently only available on some aarch64
