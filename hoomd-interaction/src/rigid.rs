47aeb8cd8e997f90bdda7315645e66443925fa7e
Alex <shihkual@umich.edu>
2025-11-11 11:12:33 -0500

    Add virial correction due to constraint.

--- hoomd-interaction/src/rigid.rs
+++ hoomd-interaction/src/rigid.rs
@@ -9,16 +9,20 @@
 //!
 //! TODO: Expand documentation.
 
-use std::ops::AddAssign;
+use std::ops::{AddAssign, Sub};
 
+use crate::{
+    NetBodyForce, NetBodyForceAndTorque, NetBodyForceAndVirial, NetBodyTorque, SiteForce,
+    SiteForceAndVirial,
+};
 use hoomd_microstate::{
     Microstate, Transform,
     property::{Orientation, Position},
 };
 use hoomd_vector::{
-    Angle, Cartesian, Cross, Rotate, Rotation, RotationMatrix, TensorProduct, Vector, Versor, WedgeProduct
+    Angle, Cartesian, Cross, Rotate, Rotation, RotationMatrix, TensorProduct, Vector, Versor,
+    WedgeProduct,
 };
-use crate::{NetBodyForce, NetBodyForceAndTorque, NetBodyTorque, NetBodyForceAndVirial, SiteForce, SiteForceAndVirial};
 
 pub struct Rigid<E>(pub E);
 
@@ -131,23 +135,40 @@ where
     }
 }
 
-impl<V, B, S, C, E> NetBodyForceAndVirial< V, B, S, C> for Rigid<E>
+impl<V, B, S, C, E, R> NetBodyForceAndVirial<V, B, S, C> for Rigid<E>
 where
-    V: Vector + Default + WedgeProduct + TensorProduct,
-    B: Transform<S>,
+    V: Vector + Default + TensorProduct,
+    B: Transform<S> + Orientation<Rotation = R>,
     S: Position<Position = V>,
     E: SiteForceAndVirial<V, B, S, C>,
-    V::Tensor: Default + AddAssign,
+    R: Rotate<V>,
+    V::Tensor: Default + AddAssign + Sub<Output = V::Tensor>,
 {
     /// Sum the forces on the sites to get the net force on a body.
     #[inline]
-    fn net_force_and_virial_on_body(&self, microstate: &Microstate<B, S, C>, body_index: usize) -> (V, V::Tensor) {
+    fn net_force_and_virial_on_body(
+        &self,
+        microstate: &Microstate<B, S, C>,
+        body_index: usize,
+    ) -> (V, V::Tensor) {
         let mut total_force = V::default();
         let mut total_virial = V::Tensor::default();
-        for site in microstate.iter_body_sites(body_index) {
-            let (force, virial) = self.0.net_force_and_virial_on_site(microstate, site); 
+        let q = microstate.bodies()[body_index]
+            .item
+            .properties
+            .orientation(); // the body's orientation in the system frame
+
+        for (site_index, site) in microstate.iter_body_sites(body_index).enumerate() {
+            let site_body_frame = &microstate.bodies()[body_index].item.sites[site_index];
+            let r_body_frame = site_body_frame.position(); // the site's position in the body frame (which we need in order to not have wrapping issues)
+            let r = q.rotate(r_body_frame); // the moment arm in the system frame
+            let (force, virial) = self.0.net_force_and_virial_on_site(microstate, site);
+
+            // calculate the virial correction due to rigid body constraint.
+            let virial_correction = force.tensor_product(&r);
+
             total_force += force;
-            total_virial += virial;
+            total_virial += virial - virial_correction;
         }
         (total_force, total_virial)
     }
