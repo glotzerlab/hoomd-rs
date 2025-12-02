699fc1f9c1f8ebe0f8166d2a849b15dad79ebc89
Trevor Teague <tfteague@umich.edu>
2025-12-02 14:02:37 -0500

    Appease the borrow checker.

--- hoomd-linear-algebra/src/matrix/qr.rs
+++ hoomd-linear-algebra/src/matrix/qr.rs
@@ -7,55 +7,52 @@ use super::gemv::gemv_submatrix_column_into_column;
 use super::{GeneralMatrix, Matrix};
 
 /// .
-pub(super) fn qr_decomposition<const N: usize, const M: usize>(
-    a: &Matrix<N, M>,
-) -> (Matrix<M, M>) {
+pub(super) fn qr_decomposition<const N: usize, const M: usize>(a: &Matrix<N, M>) -> (Matrix<M, M>) {
     // Return default TODO;
-    (Matrix::<M, M>::zeros(), Matrix::<N, M>::zeros())
+    (Matrix::<M, M>::zeros(), Matrix::<N, M>::zeros());
 
-    let mut A = a.copy();
-    for i in 0..N:
-        if i == (N-1){
-            let tau = 0;
-        }
-        else if let x_norm_2 = A.get_col_slice_iter(i,(i+1)..N).sum(|x| x*x){
-            if x_norm_2==0{
-            let tau = 0;
-        }
-            else{
-            let alpha = A[(i,i)];
-            let beta = - alpha.signum() * (x_norm_2 + alpha*alpha).sqrt() ;
-            //TODO: scale beta 
-            let tau = (beta - alpha)/beta;
-            for j in i+1..N{
-                A[(i,j)] /= alpha - beta;
+    let mut A = a.clone();
+    for i in 0..N {
+        let tau = 0.;
+        let beta = 1.;
+        if i == (N - 1) {
+        } else if let x_norm_2 = A
+            .get_col_slice_iter(i, (i + 1)..N)
+            .map(|x| x * x)
+            .sum::<f64>()
+        {
+            if x_norm_2 != 0.0 {
+                let alpha = A[(i, i)];
+                beta = -alpha.signum() * (x_norm_2 + alpha * alpha).sqrt();
+                //TODO: scale beta
+                tau = (beta - alpha) / beta;
+                for j in i + 1..N {
+                    A[(i, j)] /= alpha - beta;
+                }
+                //TODO: unscale beta
+                A[(i, i)] = 1.0;
             }
-            //TODO: unscale beta
-            A[(i,i)] = 1.0;
-    }
-}
-        if tau == 0{
-            continue;
         }
-        else{ 
-            let mut C = A.submatrix_slice_iter(i..N,i..M);
-            let v = A.get_col_slice_iter(i,i..M);
+        if tau == 0.0 {
+            continue;
+        } else {
+            let mut C = A.submatrix_slice_iter(i..N, i..M);
+            let v = A.get_col_slice_iter(i, i..M);
 
-            if let Some((wT, other_rows)) = C.split_first_mut(){
-                for row in other_rows{
-                    for (j, (value, v_jp1)) in row.zip(v.clone().skip()).enumerate(){
-                        first_row[j] += value * v_jp1;
-                    }
+            //let (wT, other_rows) = C.split_first_mut().unwrap();
+            let wT = C.first();
+            for row in other_rows {
+                for (j, (value, v_jp1)) in row.zip(v.clone().skip()).enumerate() {
+                    wT[j] += value * v_jp1;
                 }
             }
-            
-            for row in i..N{
-                for col in i..M{
+
+            for row in i..N {
+                for col in i..M {
                     A[(row, col)] -= tau * v[row] * wT[col];
                 }
             }
+        }
+        A[(i, i)] = beta;
     }
-    A[(i,i)] = beta;
-}
-
 }
