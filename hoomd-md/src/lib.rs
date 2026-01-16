8af607524fd1edc8be8d45e5742f35e8918c6111
Alex <shihkual@umich.edu>
2026-01-16 11:55:27 -0500

    Add trait documentation

--- hoomd-md/src/lib.rs
+++ hoomd-md/src/lib.rs
@@ -14,25 +14,31 @@ pub mod thermalize;
 pub mod thermostat;
 pub mod methods;
 
-use hoomd_microstate::Microstate;
+use hoomd_microstate::{Microstate,Body,Site,boundary,property::{
+        Position, Momentum, Orientation, AngularMomentum, NetForce, NetTorque
+}};
+use hoomd_interaction::rigid;
+use crate::thermostat::Thermostat;
+use hoomd_simulation::macrostate;
+
 
 /// Integrate over translational degrees of freedom.
 ///
 /// Conceptually, integration changes a system's [`Microstate`] according to
-/// equations of motion that are determined by the system's metric-space and
-/// degrees of freedom.
+/// equations of motion that are determined by the system's degrees of freedom.
 ///
 /// In translational integration, the equations of motion allow a body's
-/// [`Position`], [`Velocity`], and [`Acceleration`] to evolve over time.
-/// `Acceleration` only changes if the interaction model includes force, and
-/// particle properties and interactions may be additionally modulated by a
+/// [`Position`], [`Momentum`] to evolve over time.
 /// [`Thermostat`] that maintains system temperature according to the setpoint
-/// stored in a `macrostate`.
-///
-/// Mathematically, integration is accomplished using an adaptation of the
-/// symplectic and time-reversible two-step Verlet integration schemes published
-/// in Miller et al. (2002) and Kamberaj et al. (2005). Jens Glaser adapted
-/// these derivations to also accommodate constant pressure integration.
+/// stored in a [`macrostate`].
+/// 
+/// The generic type names are:
+/// * `N`: The dimension of the system.
+/// * `B`: The [`Body::properties`] type.
+/// * `S`: The [`Site::properties`] type.
+/// * `C`: The [`boundary`] condition type.
+/// * `T`: The [`Thermostat`] type.
+/// * `M`: The [`macrostate`] type.
 pub trait TranslationalMotion<B, S, C, T, M> {
     /// Perform the first integration half-step, mutating the microstate and possibly the thermostat.
     fn integrate_translation_step_one(
@@ -54,19 +60,20 @@ pub trait TranslationalMotion<B, S, C, T, M> {
 /// Integrate over rotational degrees of freedom.
 ///
 /// Conceptually, integration changes a system's [`Microstate`] according to
-/// equations of motion that are determined by the system's metric-space and
-/// degrees of freedom.
+/// equations of motion that are determined by the system's degrees of freedom.
 ///
 /// In rotational integration, the equations of motion allow a body's
-/// [`Orientation`] and [`AngularVelocity`] to evolve over time. `AngularVelocity`
-/// only changes if the interaction model includes torque, and particle properties
-/// and interactions may be additionally modulated by a [`Thermostat`] that
-/// maintains system temperature according to the setpoint stored in a `macrostate`.
-///
-/// Mathematically, integration is accomplished using an adaptation of the
-/// symplectic and time-reversible two-step Verlet integration schemes published
-/// in Miller et al. (2002) and Kamberaj et al. (2005). Jens Glaser adapted
-/// these derivations to also accommodate constant pressure integration.
+/// [`Orientation`] and [`AngularMomentum`] to evolve over time. 
+/// [`Thermostat`] that maintains system temperature according to the setpoint
+/// stored in a [`macrostate`].
+/// 
+/// The generic type names are:
+/// * `N`: The dimension of the system.
+/// * `B`: The [`Body::properties`] type.
+/// * `S`: The [`Site::properties`] type.
+/// * `C`: The [`boundary`] condition type.
+/// * `T`: The [`Thermostat`] type.
+/// * `M`: The [`macrostate`] type.
 pub trait RotationalMotion<const N: usize, B, S, C, T, M> {
     /// Perform the first integration half-step, mutating the microstate and possibly the thermostat.
     fn integrate_rotation_step_one(
@@ -85,14 +92,55 @@ pub trait RotationalMotion<const N: usize, B, S, C, T, M> {
     );
 }
 
+
+/// Update the [`NetForce`] in [`Microstate`] for each [`Body`] 
+/// using [`Position`] and [`Orientation`] stored in it.
+///
+/// The generic type names are:
+/// * `B`: The [`Body::properties`] type.
+/// * `S`: The [`Site::properties`] type.
+/// * `C`: The [`boundary`] condition type.
+/// * `E`: The interaction [`rigid`] type.
 pub trait ForceUpdate<B, S, C, E> {
+    /// Perform update.
     fn update_force(&self, microstate: &mut Microstate<B, S, C>, evaluator: &E);
 }
 
+/// Update the [`NetTorque`] in [`Microstate`] for each [`Body`] 
+/// using [`Position`] and [`Orientation`] stored in it.
+///
+/// The generic type names are:
+/// * `B`: The [`Body::properties`] type.
+/// * `S`: The [`Site::properties`] type.
+/// * `C`: The [`boundary`] condition type.
+/// * `E`: The interaction [`rigid`] type.
+/// 
+/// # Note
+/// 
+/// It will perform one extra force calculation per [`Site`] 
+/// to evaludate the [`NetTorque`]. 
+/// If updating both [`NetForce`] and [`NetTorque`] is intended, 
+/// use [`ForceAndTorqueUpdate`] for more efficient calculation that
+/// only performs force calculation once.
 pub trait TorqueUpdate<const N: usize, B, S, C, E> {
+    /// Perform update.
     fn update_torque(&self, microstate: &mut Microstate<B, S, C>, evaluator: &E);
 }
 
+/// Update the [`NetForce`] and [`NetTorque`] in [`Microstate`] 
+/// for each [`Body`] using [`Position`] and [`Orientation`] stored in it.
+///
+/// The generic type names are:
+/// * `B`: The [`Body::properties`] type.
+/// * `S`: The [`Site::properties`] type.
+/// * `C`: The [`boundary`] condition type.
+/// * `E`: The interaction [`rigid`] type.
+/// 
+/// # Note
+/// 
+/// By design, it is the most efficient implementation if updating
+/// both [`NetForce`] and [`NetTorque`] is required.
 pub trait ForceAndTorqueUpdate<const N: usize, B, S, C, E> {
+    /// Perform update.
     fn update_force_and_torque(&self, microstate: &mut Microstate<B, S, C>, evaluator: &E);
 }
