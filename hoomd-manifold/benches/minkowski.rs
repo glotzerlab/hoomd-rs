f15d7972350833c4e59ba92f292f82929b825e33
janbridley <bradleyjenj@gmail.com>
2026-02-18 16:06:29 -0500

    final one?

--- hoomd-manifold/benches/minkowski.rs
+++ hoomd-manifold/benches/minkowski.rs
@@ -9,7 +9,7 @@
 //! Benchmark Minkowski Vector
 
 use divan::{self, Bencher, black_box, counter::ItemsCount};
-use rand::{RngExt, SeedableRng, rngs::StdRng};
+use rand::{Rng, RngExt, SeedableRng, rngs::StdRng};
 
 use hoomd_manifold::{
     Hyperbolic, HyperbolicAngle, HyperbolicRotate, HyperbolicRotationMatrix, Minkowski,
