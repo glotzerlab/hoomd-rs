ae553752c840fa36a60241f0dd73287454fe96d7
Joshua A. Anderson <joaander@umich.edu>
2026-03-16 09:29:44 -0400

    Implement ParquetLogger::create_unique.

--- hoomd-utility/src/data.rs
+++ hoomd-utility/src/data.rs
@@ -10,6 +10,9 @@ use parquet::{
 use std::{fs::File, io, path::Path, sync::Arc};
 use thiserror::Error;
 
+/// The default maximum buffer size.
+const DEFAULT_MAXIMUM_BUFFER_SIZE: usize = 2_usize.pow(17);
+
 /// Enumerate possible sources of error when writing log files.
 #[non_exhaustive]
 #[derive(Error, Debug)]
@@ -21,6 +24,29 @@ pub enum Error {
     /// Encountered an IO error.
     #[error("Parquet error")]
     Parquet(#[from] parquet::errors::ParquetError),
+}
+
+/// Create a unique file by appending an integer index.
+///
+/// The first file created will be "{path}.1". If that exists,
+/// the function will create "{path}.2" and so on.
+fn create_unique_file<P: AsRef<Path>>(path: P) -> io::Result<File> {
+    let mut index: u32 = 0;
+
+    loop {
+        let numbered_path = path.as_ref().with_added_extension(index.to_string());
+
+        match File::create_new(numbered_path) {
+            Ok(file) => return Ok(file),
+            Err(error) => {
+                match error.kind() {
+                    io::ErrorKind::AlreadyExists => (),
+                    _ => return Err(error),
+            }}
+        }
+
+        index += 1;
+    }
 }
 
 /// Write log records to a Parquet data file.
@@ -134,7 +160,66 @@ where
         Ok(Self {
             writer,
             buffer,
-            maximum_buffer_size: 2_usize.pow(17),
+            maximum_buffer_size: DEFAULT_MAXIMUM_BUFFER_SIZE,
+        })
+    }
+
+    /// Create a unique parquet file.
+    ///
+    /// Parquet files cannot be appended to once written. The accepted solution
+    /// by the Parquet developers is to create many files (`file.parquet.0`,
+    /// `file.parquet.1`, and so on) that you concatenate on read.
+    ///
+    /// `create_unique` facilitates this process by appending `.0`, `.1`, ... `.{N}`.
+    /// to the given `path`. `create_unique` attempts all extensions in order,
+    /// continuing to the next increment each time it finds an existing file.
+    ///
+    /// The default [`maximum_buffer_size`] is $` 2^{17} `$ records.
+    ///
+    /// [`maximum_buffer_size`]: Self::maximum_buffer_size
+    ///
+    /// # Errors
+    ///
+    /// Returns [`Error`] when there is I/O error opening the file
+    /// or `parquet` fails to initialize the file.
+    ///
+    /// [`Error`]: enum@crate::data::Error
+    ///
+    /// # Example
+    /// ```
+    /// use hoomd_utility::data::ParquetLogger;
+    /// use parquet_derive::ParquetRecordWriter;
+    ///
+    /// #[derive(ParquetRecordWriter)]
+    /// pub struct LogRecord {
+    ///     /// The simulation step.
+    ///     step: u64,
+    ///
+    ///     /// Total system potential energy.
+    ///     potential_energy: f64,
+    /// }
+    ///
+    /// # fn main() -> Result<(), Box<dyn std::error::Error>> {
+    /// # use tempfile::tempdir;
+    /// # let tmp_dir = tempdir().expect("temp dir should be created");
+    /// # let path = tmp_dir.path().join("log.parquet");
+    /// // let path = "log.parquet";
+    /// let mut parquet_logger = ParquetLogger::<LogRecord>::create_unique(path)?;
+    /// # Ok(())
+    /// # }
+    /// ```
+    #[inline]
+    pub fn create_unique<P: AsRef<Path>>(path: P) -> Result<Self, Error> {
+        let buffer = Vec::<T>::new();
+        let schema = buffer.as_slice().schema()?;
+        let props = Arc::new(WriterProperties::builder().build());
+        let log_file = create_unique_file(path)?;
+        let writer = SerializedFileWriter::new(log_file, schema, props)?;
+
+        Ok(Self {
+            writer,
+            buffer,
+            maximum_buffer_size: DEFAULT_MAXIMUM_BUFFER_SIZE,
         })
     }
 
@@ -246,5 +331,45 @@ where
     fn drop(&mut self) {
         let _ = self.sync();
         let _ = self.writer.finish();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    use parquet_derive::ParquetRecordWriter;
+    use tempfile::tempdir;
+    use assert2::check;
+    use super::*;
+
+    #[derive(ParquetRecordWriter)]
+    struct LogRecord {
+        a: f64,
+    }
+
+    #[test]
+    fn test_create_unique() -> anyhow::Result<()> {            
+        let tmp_dir = tempdir()?;
+        let path = tmp_dir.path().join("test.parquet");
+        let _ = ParquetLogger::create_unique(path.clone())?;
+
+        check!(fs::exists(path.with_added_extension("0"))?);
+        check!(!fs::exists(path.with_added_extension("1"))?);
+        check!(!fs::exists(path.with_added_extension("2"))?);
+
+        let _ = ParquetLogger::create_unique(path.clone())?;
+
+        check!(fs::exists(path.with_added_extension("0"))?);
+        check!(fs::exists(path.with_added_extension("1"))?);
+        check!(!fs::exists(path.with_added_extension("2"))?);
+
+        let _ = ParquetLogger::create_unique(path.clone())?;
+
+        check!(fs::exists(path.with_added_extension("0"))?);
+        check!(fs::exists(path.with_added_extension("1"))?);
+        check!(fs::exists(path.with_added_extension("2"))?);
+
+        Ok(())
     }
 }
