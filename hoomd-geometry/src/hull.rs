48dff0ba1250e1c0c24599f56b47aec2cef32fcd
janbridley <bradleyjenj@gmail.com>
2026-03-25 16:28:12 -0400

    fixqtests

--- hoomd-geometry/src/hull.rs
+++ hoomd-geometry/src/hull.rs
@@ -7,11 +7,11 @@ struct _SortKey {
 }
 
 /// Find the lowest, leftmost point from a slice of Cartesian vectors.
-fn _find_lowest_leftmost(vertices: &[Cartesian<2>]) -> (usize, Cartesian<2>) {
+fn find_lowest_leftmost(vertices: &[Cartesian<2>]) -> (usize, Cartesian<2>) {
     vertices.iter().enumerate().fold(
         (usize::MAX, Cartesian::from([f64::INFINITY, f64::INFINITY])),
         |(i, p), (min_i, &min_p)| {
-            if p[1] < min_p[1] || (p[1] == min_p[0] && p[0] < min_p[0]) {
+            if p[1] < min_p[1] || (p[1] == min_p[1] && p[0] < min_p[0]) {
                 (i, p)
             } else {
                 (min_i, min_p)
@@ -27,4 +27,44 @@ fn _find_lowest_leftmost(vertices: &[Cartesian<2>]) -> (usize, Cartesian<2>) {
 pub fn hull_2d_grahamscan<I>(vertices: Vec<Cartesian<2>>) -> Result<bool, Error> {
     // vertices.sort_by(|a, b| {});
     Ok(true)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use approxim::assert_relative_eq;
+    use rstest::*;
+
+    #[rstest]
+    #[case::single_point(vec![[1.0, 2.0]], 0, [1.0, 2.0])]
+    #[case::two_points_first_lower(vec![[1.0, 1.0], [2.0, 3.0]], 0, [1.0, 1.0])]
+    #[case::two_points_second_lower(vec![[1.0, 3.0], [2.0, 1.0]], 1, [2.0, 1.0])]
+    #[case::same_y_leftmost_wins(vec![[3.0, 1.0], [1.0, 1.0], [2.0, 1.0]], 1, [1.0, 1.0])]
+    #[case::same_y_negative(vec![[0.0, -5.0], [-3.0, -5.0], [2.0, -5.0]], 1, [-3.0, -5.0])]
+    #[case::multiple_points(vec![[3.0, 5.0], [1.0, 1.0], [4.0, 2.0], [2.0, 3.0]], 1, [1.0, 1.0])]
+    #[case::negative_coords(vec![[0.0, 0.0], [-1.0, -1.0], [1.0, -1.0]], 1, [-1.0, -1.0])]
+    #[case::same_y_all_negative_x(vec![[5.0, 0.0], [-10.0, 0.0], [-5.0, 0.0]], 1, [-10.0, 0.0])]
+    #[case::lowest_is_only_point(vec![[100.0, -100.0]], 0, [100.0, -100.0])]
+    #[case::diagonal_tiebreak(vec![[5.0, 5.0], [4.0, 4.0], [3.0, 3.0], [2.0, 2.0], [1.0, 1.0]], 4, [1.0, 1.0])]
+    fn test_find_lowest_leftmost(
+        #[case] vertices: Vec<[f64; 2]>,
+        #[case] expected_idx: usize,
+        #[case] expected_point: [f64; 2],
+    ) {
+        let vertices: Vec<Cartesian<2>> = vertices.into_iter().map(Cartesian::from).collect();
+        let (idx, point) = find_lowest_leftmost(&vertices);
+        assert_eq!(idx, expected_idx);
+        assert_relative_eq!(point, Cartesian::from(expected_point));
+    }
+
+    #[rstest]
+    fn test_single_point_various_coords(
+        #[values([0.0, 0.0], [-1.0, -1.0], [100.0, -50.0], [f64::MIN_POSITIVE, f64::MIN_POSITIVE])]
+        coords: [f64; 2],
+    ) {
+        let vertices = vec![Cartesian::from(coords)];
+        let (idx, point) = find_lowest_leftmost(&vertices);
+        assert_eq!(idx, 0);
+        assert_relative_eq!(point, Cartesian::from(coords));
+    }
 }
