9f65cfa7a5557ce90b9c0856cb16307ca57cfcd3
Trevor Teague <tfteague@umich.edu>
2026-04-10 11:44:44 -0400

    work on ghosts

--- hoomd-microstate/src/boundary/periodic/triclinic.rs
+++ hoomd-microstate/src/boundary/periodic/triclinic.rs
@@ -42,9 +42,9 @@ where
     }
 }
 
-impl<S> GenerateGhosts<S> for Periodic<Hypercuboid<2>>
+impl<S> GenerateGhosts<S> for Periodic<Triclinic>
 where
-    S: Position<Vector = Cartesian<2>> + Copy + Default,
+    S: Position<Vector = Cartesian<3>> + Copy + Default,
 {
     #[inline]
     fn maximum_interaction_range(&self) -> f64 {
@@ -52,8 +52,119 @@ where
     }
 
     #[inline]
+    /// Place periodic images of sites near the edge of the periodic boundary.
+    ///
+    /// For triclinic boxes, `generate_ghosts` places ghosts near the 6 faces, 12 edges,
+    /// and 8 vertices of the box.
     fn generate_ghosts(&self, site_properties: &S) -> ArrayVec<S, MAX_GHOSTS> {
-        todo!();
+        let mut result = ArrayVec::new();
+
+        let r = site_properties.position();
+        let max = self.shape.maximal_extents();
+        let min = self.shape.minimal_extents();
+
+        if !self.shape.is_point_inside(r) {
+            return result;
+        }
+
+        let new_site = |x, y, z| {
+            let mut new_site = *site_properties;
+            new_site.position_mut()[0] += x * self.shape.edge_lengths[0].get(); // Todo: convert tiltfactor/extents
+            new_site.position_mut()[1] += y * self.shape.edge_lengths[1].get();
+            new_site.position_mut()[2] += z * self.shape.edge_lengths[2].get();
+            new_site
+        };
+
+        let near_left = r[0] < min[0] + self.maximum_interaction_range;
+        let near_right = r[0] > max[0] - self.maximum_interaction_range;
+        let near_top = r[1] > max[1] - self.maximum_interaction_range;
+        let near_bottom = r[1] < min[1] + self.maximum_interaction_range;
+        let near_front = r[2] > max[2] - self.maximum_interaction_range;
+        let near_back = r[2] < min[2] + self.maximum_interaction_range;
+
+        if near_right {
+            result.push(new_site(-1.0, 0.0, 0.0));
+        }
+        if near_left {
+            result.push(new_site(1.0, 0.0, 0.0));
+        }
+        if near_top {
+            result.push(new_site(0.0, -1.0, 0.0));
+        }
+        if near_bottom {
+            result.push(new_site(0.0, 1.0, 0.0));
+        }
+        if near_front {
+            result.push(new_site(0.0, 0.0, -1.0));
+        }
+        if near_back {
+            result.push(new_site(0.0, 0.0, 1.0));
+        }
+
+        if near_right && near_top {
+            result.push(new_site(-1.0, -1.0, 0.0));
+        }
+        if near_right && near_bottom {
+            result.push(new_site(-1.0, 1.0, 0.0));
+        }
+        if near_right && near_front {
+            result.push(new_site(-1.0, 0.0, -1.0));
+        }
+        if near_right && near_back {
+            result.push(new_site(-1.0, 0.0, 1.0));
+        }
+        if near_left && near_top {
+            result.push(new_site(1.0, -1.0, 0.0));
+        }
+        if near_left && near_bottom {
+            result.push(new_site(1.0, 1.0, 0.0));
+        }
+        if near_left && near_front {
+            result.push(new_site(1.0, 0.0, -1.0));
+        }
+        if near_left && near_back {
+            result.push(new_site(1.0, 0.0, 1.0));
+        }
+
+        if near_top && near_front {
+            result.push(new_site(0.0, -1.0, -1.0));
+        }
+        if near_bottom && near_front {
+            result.push(new_site(0.0, 1.0, -1.0));
+        }
+        if near_top && near_back {
+            result.push(new_site(0.0, -1.0, 1.0));
+        }
+        if near_bottom && near_back {
+            result.push(new_site(0.0, 1.0, 1.0));
+        }
+
+        if near_right && near_top && near_front {
+            result.push(new_site(-1.0, -1.0, -1.0));
+        }
+        if near_right && near_top && near_back {
+            result.push(new_site(-1.0, -1.0, 1.0));
+        }
+        if near_right && near_bottom && near_front {
+            result.push(new_site(-1.0, 1.0, -1.0));
+        }
+        if near_right && near_bottom && near_back {
+            result.push(new_site(-1.0, 1.0, 1.0));
+        }
+        if near_left && near_top && near_front {
+            result.push(new_site(1.0, -1.0, -1.0));
+        }
+        if near_left && near_top && near_back {
+            result.push(new_site(1.0, -1.0, 1.0));
+        }
+        if near_left && near_bottom && near_front {
+            result.push(new_site(1.0, 1.0, -1.0));
+        }
+        if near_left && near_bottom && near_back {
+            result.push(new_site(1.0, 1.0, 1.0));
+        }
+
+        result
     }
 }
 
