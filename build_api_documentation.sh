dea041f4d96e5299dd02158aa5e63543eda417b2
Joshua A. Anderson <joaander@umich.edu>
2026-04-27 12:37:07 -0400

    Use full name for google analytics.

--- build_api_documentation.sh
+++ build_api_documentation.sh
@@ -10,7 +10,7 @@
 
 set -euo pipefail
 
-export RUSTDOCFLAGS="--html-in-header ga.html --html-in-header katex.html"
+export RUSTDOCFLAGS="--html-in-header google_analytics.html --html-in-header katex.html"
 for package in derive linear-algebra simulation utility rand vector gsd manifold spatial \
                geometry microstate interaction mc bevy md
 do
