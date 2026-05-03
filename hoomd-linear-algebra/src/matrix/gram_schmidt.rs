bb0244da989f97182e59507e02b4dab8c4305722
janbridley <bradleyjenj@gmail.com>
2026-05-03 19:37:15 -0400

    Working! Correct?

--- hoomd-linear-algebra/src/matrix/gram_schmidt.rs
+++ hoomd-linear-algebra/src/matrix/gram_schmidt.rs
@@ -1,6 +1,6 @@
 use crate::matrix::Matrix;
 
-///
+/// Construct an orthonormal basis from the vectors in a [`Matrix`].
 ///
 /// Implementation based on <https://www.sfu.ca/~jtmulhol/py4math/linalg/np-gramschmidt/>
 #[must_use]
@@ -12,43 +12,28 @@ pub fn gram_schmidt<const N: usize, const M: usize>(a: &Matrix<N, M>) -> Matrix<
         // the previous orthogonal vectors.
         for k in (0..j) {
             let j_dot_k = a
-                .get_col_slice_iter(k, 0..N)
-                .zip(a.get_col_slice_iter(j, 0..N))
+                .get_col(k)
+                .iter_elements()
+                .zip(a.get_col(j).iter_elements())
                 .fold(0.0, |acc, (l, r)| acc + (l * r));
             // Apply the projection
             for i in 0..N {
                 a[(i, j)] -= a[(i, k)] * j_dot_k;
             }
-            // # If original vectors aren't lin indep then we can check for this:
-            // #
-            // if np.isclose(np.linalg.norm(A[:, j]), 0, rtol=1e-15, atol=1e-14, equal_nan=False):
-            //     A[:, j] = np.zeros(A.shape[0])
-            // else:
-            //     A[:, j] = A[:, j] / np.linalg.norm(A[:, j])
             let column_j_norm = a
                 .get_col(j)
                 .iter_elements()
                 .fold(0.0, |acc, x| acc + x * x)
                 .sqrt();
+
+            // If the initial vectors are not linearly independent, zero out the col.
             if column_j_norm.is_finite() {
                 a.get_col_slice_iter_mut(j, 0..N)
                     .for_each(|x| *x /= column_j_norm);
+            } else {
+                a.get_col_slice_iter_mut(j, 0..N).for_each(|x| *x = 0.0);
             }
         }
     }
     a
 }
-
-// def gram_schmidt(A):
-//     for j in range(n):
-//         # For the vector in column j, find the perpendicular
-//         # of the projection onto the previous orthogonal vectors.
-//         for k in range(j):
-//             A[:, j] -= np.dot(A[:, k], A[:, j]) * A[:, k]
-//         # If original vectors aren't lin indep then we can check for this:
-//         #
-//         if np.isclose(np.linalg.norm(A[:, j]), 0, rtol=1e-15, atol=1e-14, equal_nan=False):
-//             A[:, j] = np.zeros(A.shape[0])
-//         else:
-//             A[:, j] = A[:, j] / np.linalg.norm(A[:, j])
-//     return A
