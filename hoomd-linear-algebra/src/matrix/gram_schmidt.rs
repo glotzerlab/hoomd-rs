659e55f6d6dde29de8c7d2539e65b4343cf4fd9e
janbridley <bradleyjenj@gmail.com>
2026-05-03 19:59:53 -0400

    add tests

--- hoomd-linear-algebra/src/matrix/gram_schmidt.rs
+++ hoomd-linear-algebra/src/matrix/gram_schmidt.rs
@@ -41,3 +41,85 @@ pub fn gram_schmidt<const N: usize, const M: usize>(a: &Matrix<N, M>) -> Matrix<
     } // end loop over j
     a
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::matrix::Matrix;
+    use crate::matrix::test_utils::assert_matrixes_ulps_eq;
+
+    #[test]
+    fn test_gram_schmidt_3x3() {
+        // Example 1 from https://www.sfu.ca/~jtmulhol/py4math/linalg/np-gramschmidt/
+        let a = Matrix::<3, 3> {
+            rows: [[1.0, -1.0, 0.0], [1.0, 2.0, 1.0], [0.0, 1.0, 1.0]],
+        };
+        let q = gram_schmidt(&a);
+
+        let sqrt2 = 2.0f64.sqrt();
+        let sqrt11 = 11.0f64.sqrt();
+        let sqrt22 = 22.0f64.sqrt();
+
+        let expected = Matrix::<3, 3> {
+            rows: [
+                [1.0 / sqrt2, -3.0 / sqrt22, 1.0 / sqrt11],
+                [1.0 / sqrt2, 3.0 / sqrt22, -1.0 / sqrt11],
+                [0.0, 2.0 / sqrt22, 3.0 / sqrt11],
+            ],
+        };
+
+        assert_matrixes_ulps_eq::<3, 3, _, _>(&q, &expected);
+    }
+
+    #[test]
+    fn test_gram_schmidt_linearly_dependent() {
+        // Example 2 from https://www.sfu.ca/~jtmulhol/py4math/linalg/np-gramschmidt/
+        // A = [[1, 1, 2, 1], [1, 0, 1, 0], [0, 1, 1, 0], [1, 1, 2, 1]] (columns)
+        let a = Matrix::<4, 4> {
+            rows: [
+                [1.0, 1.0, 2.0, 1.0],
+                [1.0, 0.0, 1.0, 0.0],
+                [0.0, 1.0, 1.0, 0.0],
+                [1.0, 1.0, 2.0, 1.0],
+            ],
+        };
+        let q = gram_schmidt(&a);
+
+        let sqrt3 = 3.0f64.sqrt();
+        let sqrt15 = 15.0f64.sqrt();
+        let sqrt10 = 10.0f64.sqrt();
+
+        let expected = Matrix::<4, 4> {
+            rows: [
+                [1.0 / sqrt3, 1.0 / sqrt15, 0.0, 1.0 / sqrt10],
+                [1.0 / sqrt3, -2.0 / sqrt15, 0.0, -2.0 / sqrt10],
+                [0.0, 3.0 / sqrt15, 0.0, -2.0 / sqrt10],
+                [1.0 / sqrt3, 1.0 / sqrt15, 0.0, 1.0 / sqrt10],
+            ],
+        };
+
+        assert_matrixes_ulps_eq::<4, 4, _, _>(&q, &expected);
+    }
+
+    #[test]
+    fn test_gram_schmidt_subspace() {
+        // Example 3 from https://www.sfu.ca/~jtmulhol/py4math/linalg/np-gramschmidt/
+        let a = Matrix::<3, 2> {
+            rows: [[1.0, -1.0], [1.0, 2.0], [0.0, 1.0]],
+        };
+        let q = gram_schmidt(&a);
+
+        let sqrt2 = 2.0f64.sqrt();
+        let sqrt22 = 22.0f64.sqrt();
+
+        let expected = Matrix::<3, 2> {
+            rows: [
+                [1.0 / sqrt2, -3.0 / sqrt22],
+                [1.0 / sqrt2, 3.0 / sqrt22],
+                [0.0, 2.0 / sqrt22],
+            ],
+        };
+
+        assert_matrixes_ulps_eq::<3, 2, _, _>(&q, &expected);
+    }
+}
