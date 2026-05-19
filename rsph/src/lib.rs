c669c5c7fe9163b631c51f057891a12e4bc65295
janbridley <bradleyjenj@gmail.com>
2026-05-19 14:49:59 -0400

    clippy --fix

--- rsph/src/lib.rs
+++ rsph/src/lib.rs
@@ -215,7 +215,8 @@ impl<const L: usize> Index<usize> for HarmonicOutput<L> {
 
 impl<const L: usize> IntoIterator for HarmonicOutput<L> {
     type Item = Complex64;
-    type IntoIter = std::iter::Chain<std::iter::Once<Complex64>, std::array::IntoIter<Complex64, L>>;
+    type IntoIter =
+        std::iter::Chain<std::iter::Once<Complex64>, std::array::IntoIter<Complex64, L>>;
 
     #[inline]
     fn into_iter(self) -> Self::IntoIter {
