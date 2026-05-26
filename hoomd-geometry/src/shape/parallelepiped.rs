b1323af587762c5d3995fadb48167c94566a855e
Trevor Teague <tfteague@umich.edu>
2026-05-26 10:44:14 -0400

    clip

--- hoomd-geometry/src/shape/parallelepiped.rs
+++ hoomd-geometry/src/shape/parallelepiped.rs
@@ -1,6 +1,7 @@
 // Copyright (c) 2024-2026 The Regents of the University of Michigan.
 // Part of hoomd-rs, released under the BSD 3-Clause License.
 
+//! Implement [`Hyperparallelepiped`]
 use std::array;
 
 use hoomd_linear_algebra::matrix::{
