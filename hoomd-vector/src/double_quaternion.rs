fb80995320354c26f85890572a2ee4cf7937171d
janbridley <bradleyjenj@gmail.com>
2026-06-11 12:13:02 -0400

    Correct text

--- hoomd-vector/src/double_quaternion.rs
+++ hoomd-vector/src/double_quaternion.rs
@@ -2,9 +2,7 @@ use crate::{Cartesian, Quaternion, Rotate, RotationMatrix, Versor};
 
 /// A pair of [`Versor`]s that represent a 4D rotation.
 ///
-/// Each [`Versor`] represents an independent rotation about a plane in R^4. We assume
-/// that the left part of the rotation is about the `XY` plane and the right part is
-/// about the `ZW plane`.
+/// Each [`Versor`] represents an independent rotation about a plane in R^4.
 pub struct DoubleVersor {
     /// The left-isoclinic part of the rotation.
     l: Versor,
