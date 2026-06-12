b40cff033cbfd5691805750b9a769dc876742ccb
janbridley <bradleyjenj@gmail.com>
2026-06-12 16:22:13 -0400

    unit edge length to match others

--- hoomd-geometry/src/shape/convex_polytope.rs
+++ hoomd-geometry/src/shape/convex_polytope.rs
@@ -382,7 +382,7 @@ impl<const MAX_VERTICES: usize> ConvexPolytope<4, MAX_VERTICES> {
     #[must_use]
     /// Create a 24-cell, a regular polychoron unique to four dimensions.
     ///
-    /// This shape's vertices are defined by the permutations of `{±1, ±1, 0, 0}`.
+    /// This shape's vertices are defined by the permutations of `{±1/√2, ±1/√2, 0, 0}`.
     ///
     /// # Example
     /// ```
@@ -393,23 +393,26 @@ impl<const MAX_VERTICES: usize> ConvexPolytope<4, MAX_VERTICES> {
     /// # fn main() -> Result<(), hoomd_geometry::Error> {
     /// let octaplex = ConvexPolytope::<4, 24>::octaplex();
     /// assert_eq!(octaplex.vertices().len(), 24);
-    /// assert_eq!(octaplex.bounding_sphere_radius().get(), f64::sqrt(2.0));
+    /// assert_relative_eq!(octaplex.bounding_sphere_radius().get(), 1.0);
     ///
     /// // All vertices are the same distance from the origin
-    /// assert!(octaplex.vertices().iter().all(|v| v.norm_squared() == 2.0));
+    /// assert!(octaplex.vertices().iter().all(|v| (v.norm_squared() - 1.0).abs() < 1e-10));
     /// # Ok(())
     /// # }
     /// ```
-    #[expect(clippy::missing_panics_doc, reason = "sqrt(2) is positive")]
+    #[expect(clippy::missing_panics_doc, reason = "hard-coded constant is positive")]
     pub fn octaplex() -> Self {
+        let s = std::f64::consts::FRAC_1_SQRT_2;
         let mut vertices = ArrayVec::new();
 
         for (l, r) in [(0, 1), (0, 2), (0, 3), (1, 2), (1, 3), (2, 3)] {
             for ls in [-1.0, 1.0] {
                 for rs in [-1.0, 1.0] {
                     vertices.push(
-                        std::array::from_fn(|i| f64::from(i == l) * ls + f64::from(i == r) * rs)
-                            .into(),
+                        std::array::from_fn(|i| {
+                            (f64::from(i == l) * ls + f64::from(i == r) * rs) * s
+                        })
+                        .into(),
                     );
                 }
             }
@@ -417,7 +420,7 @@ impl<const MAX_VERTICES: usize> ConvexPolytope<4, MAX_VERTICES> {
 
         Self {
             vertices,
-            bounding_radius: SQRT_2.try_into().expect("positive"),
+            bounding_radius: 1.0_f64.try_into().expect("hard-coded constant is positive"),
         }
     }
 }
