58411b3b13aad71daeada7fbfde918f2a93f442f
janbridley <bradleyjenj@gmail.com>
2026-06-15 14:20:40 -0400

    clippy

--- hoomd-vector/src/cartesian.rs
+++ hoomd-vector/src/cartesian.rs
@@ -20,7 +20,7 @@ use rand::{
 use crate::{Cross, Error, InnerProduct, Metric, Rotate, Unit, Vector};
 use hoomd_linear_algebra::{
     MatMul,
-    matrix::{Matrix, Matrix33},
+    matrix::Matrix,
 };
 
 /// A [`Vector`] represented by `N` `f64` coordinates.
