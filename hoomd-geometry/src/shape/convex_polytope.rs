662ad78d484dda29c4cfec50357e9424196d23ea
janbridley <bradleyjenj@gmail.com>
2026-06-18 20:47:09 -0400

    Revert "Minor changes to f32 support"
    This reverts commit 3ef1f33bf1edab74d574a3181b4ef69ebdb01e11.

--- hoomd-geometry/src/shape/convex_polytope.rs
+++ hoomd-geometry/src/shape/convex_polytope.rs
@@ -394,19 +394,18 @@ impl<const N: usize, const MAX_VERTICES: usize> SupportMapping<Cartesian<N>>
             1 => self.vertices[0],
             _ => {
                 let n_f32 = std::array::from_fn(|i| n[i] as f32);
-
-                let support = self
+                let vertices_f32 = self
                     .vertices
                     .iter()
-                    .map(|v| {
-                        let v_f32 = v.coordinates.map(|x| x as f32);
-                        let dot = dot_arrays(&v_f32, &n_f32);
-                        (v_f32, dot)
+                    .map(|v| v.coordinates.map(|x| x as f32));
+                let support = vertices_f32
+                    .max_by(|a, b| {
+                        dot_arrays(a, &n_f32)
+                            .partial_cmp(&dot_arrays(b, &n_f32))
+                            .unwrap_or(std::cmp::Ordering::Equal)
                     })
-                    .max_by(|a, b| a.1.partial_cmp(&b.1).unwrap_or(std::cmp::Ordering::Equal))
-                    .expect("N > 1 is guaranteed by match statement");
-
-                support.0.map(f64::from).into()
+                    .expect("the 0 match statement should handle empty vectors");
+                support.map(f64::from).into()
             }
         }
     }
