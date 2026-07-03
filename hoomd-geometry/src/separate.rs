5415390e60c9c694e3a147d79d80fd648d58f662
janbridley <bradleyjenj@gmail.com>
2026-07-03 13:05:57 -0400

    rotate

--- hoomd-geometry/src/separate.rs
+++ hoomd-geometry/src/separate.rs
@@ -1,5 +1,5 @@
 use crate::shape::Hypersphere;
-use hoomd_vector::{Cartesian, InnerProduct, Vector};
+use hoomd_vector::{Cartesian, InnerProduct, Rotate, Vector};
 use thiserror::Error;
 
 #[non_exhaustive]
@@ -10,28 +10,44 @@ pub enum PenetrationError {
     DoesNotIntersect,
 }
 
-pub trait ShapePenetration<const N: usize, V: Vector + InnerProduct> {
+pub trait ShapePenetration<const N: usize, V: Vector + InnerProduct, R: Rotate<V>> {
     /// .
     const TOLERANCE: f64 = 1e-12;
 
     /// .
     ///
     /// Sep function will come up with an initial guess
-    fn penetration_vector<A, B>(a: &A, b: &B) -> Result<V, PenetrationError>;
-
-    /// .
-    fn penetration_vector_from_guess<A, B>(a: &A, b: &B, guess: &V) -> Result<V, PenetrationError>;
-}
-
-impl<const N: usize> ShapePenetration<N, Cartesian<N>> for Hypersphere<N> {
-    fn penetration_vector<A, B>(a: &A, b: &B) -> Result<Cartesian<N>, PenetrationError> {
-        todo!()
-    }
-    fn penetration_vector_from_guess<A, B>(
+    fn penetration_vector<A, B>(
         a: &A,
         b: &B,
-        guess: &Cartesian<N>,
+        v_a: V,
+        o_a: R,
+        v_b: V,
+        o_b: R,
+    ) -> Result<V, PenetrationError>;
+
+    // /// .
+    // fn penetration_vector_from_guess<A, B>(a: &A, b: &B, guess: &V) -> Result<V, PenetrationError>;
+}
+
+impl<const N: usize, R: Rotate<Cartesian<N>>> ShapePenetration<N, Cartesian<N>, R>
+    for Hypersphere<N>
+{
+    fn penetration_vector<A, B>(
+        a: &A,
+        b: &B,
+        v_a: Cartesian<N>,
+        o_a: R,
+        v_b: Cartesian<N>,
+        o_b: R,
     ) -> Result<Cartesian<N>, PenetrationError> {
         todo!()
     }
+    // fn penetration_vector_from_guess<A, B>(
+    //     a: &A,
+    //     b: &B,
+    //     guess: &Cartesian<N>,
+    // ) -> Result<Cartesian<N>, PenetrationError> {
+    //     todo!()
+    // }
 }
