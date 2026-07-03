744fec01ebd25fe8818b82394d000e604de69798
janbridley <bradleyjenj@gmail.com>
2026-07-03 12:57:39 -0400

    working with errors

--- hoomd-geometry/src/separate.rs
+++ hoomd-geometry/src/separate.rs
@@ -1,14 +1,37 @@
+use crate::shape::Hypersphere;
 use hoomd_vector::{Cartesian, InnerProduct, Vector};
+use thiserror::Error;
 
-pub trait SeparationDistance<const N: usize, V: Vector + InnerProduct> {
+#[non_exhaustive]
+#[derive(Error, PartialEq, Debug)]
+pub enum PenetrationError {
+    /// A penetration is only well-defined if the shapes intersect
+    #[error("Shapes do not intersect.")]
+    DoesNotIntersect,
+}
+
+pub trait ShapePenetration<const N: usize, V: Vector + InnerProduct> {
     /// .
-    const TOLERANCE: f64;
+    const TOLERANCE: f64 = 1e-12;
 
     /// .
     ///
     /// Sep function will come up with an initial guess
-    fn separating_vector<A, B>(a: &A, v: &B) -> V;
+    fn penetration_vector<A, B>(a: &A, v: &B) -> Result<V, PenetrationError>;
 
     /// .
-    fn separating_vector_from_guess<A, B>(a: &A, b: &B, guess: &V);
+    fn penetration_vector_from_guess<A, B>(a: &A, b: &B, guess: &V) -> Result<V, PenetrationError>;
+}
+
+impl<const N: usize> ShapePenetration<N, Cartesian<N>> for Hypersphere<N> {
+    fn penetration_vector<A, B>(a: &A, b: &B) -> Result<Cartesian<N>, PenetrationError> {
+        todo!()
+    }
+    fn penetration_vector_from_guess<A, B>(
+        a: &A,
+        b: &B,
+        guess: &Cartesian<N>,
+    ) -> Result<Cartesian<N>, PenetrationError> {
+        todo!()
+    }
 }
