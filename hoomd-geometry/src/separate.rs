9532b7e36fde115e612ff2c7bfd77cf25cb05730
janbridley <bradleyjenj@gmail.com>
2026-07-03 12:45:15 -0400

    template trait

--- hoomd-geometry/src/separate.rs
+++ hoomd-geometry/src/separate.rs
@@ -1,1 +1,14 @@
+use hoomd_vector::{Cartesian, InnerProduct, Vector};
 
+pub trait SeparationDistance<const N: usize, V: Vector + InnerProduct> {
+    /// .
+    const TOLERANCE: f64;
+
+    /// .
+    ///
+    /// Sep function will come up with an initial guess
+    fn separating_vector<A, B>(a: &A, v: &B) -> V;
+
+    /// .
+    fn separating_vector_from_guess<A, B>(a: &A, b: &B, guess: &V);
+}
