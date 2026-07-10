9e38790b07ce469d85924a90352e1239966b7a9a
Joseph Burkhart <61951318+josephburkhart@users.noreply.github.com>
2026-07-10 10:55:44 -0400

    Initial sketch of Langevin

---
+++ hoomd-md/src/method/langevin.rs
@@ -0,0 +1,337 @@
+// Copyright (c) 2024-2026 The Regents of the University of Michigan.
+// Part of hoomd-rs, released under the BSD 3-Clause License.
+
+//! Implement `Langevin`.
+
+use hoomd_microstate::Site;
+use crate::{method::{Gamma, GammaR}, thermostat::NoThermostat};
+
+/// Integrate bodies' degrees of freedom in the microstate according to
+/// Langevin equations of motion, modelling the NVE or NVT ensemble.
+/// 
+/// The `Langevin` implementation follows the same symplectic integration scheme
+/// used in [`ConstantVolume`](crate::ConstantVolume), but with drag and random
+/// forces and torques applied to each body *i*:
+/// 
+/// ```math
+/// \begin{align*}
+/// \vec{F}_i &= \vec{F}_C - \gamma \cdot \vec{v}_i + \vec{F}_R \\
+/// \vec{\tau}_i &= \vec{\tau}_C - \vec{\gamma}_R \cdot \vec{\omega}_i + \vec{\tau}_R \\
+/// \end{align*}
+/// ```
+/// 
+/// where $` \vec{F}_C `$ and $` \vec{\tau}_C `$ are the force and torque on the
+/// body from all other bodies and external interactions, $` \gamma `$ and
+/// $` \vec{\gamma}_R `$ are the translational and rotational drag coefficients, and
+/// $` \vec{F}_R `$ and $` \vec{\tau}_R `$ are random forces and torques. These
+/// random forces and torques are uniform
+/// 
+/// ```math
+/// \begin{align*}
+/// \left< \vec{F}_R \right> &= 0 \\
+/// \left< \vec{\tau}_R \right> &= 0 \\
+/// \end{align*}
+/// ```
+/// 
+/// and their magnitudes are chosen via the [fluctuation-dissipation theorem]
+/// to be consistent with the specified drag and temperature
+/// 
+/// ```math
+/// \begin{align*}
+/// \left< \left| \vec{F}_R \right|^2 \right> &= \frac{2 d k T \gamma}{\Delta t} \\
+/// \left< \left| \vec{\tau}_R \right|^2 \right> &= \frac{2 d_R k T \gamma_R}{\Delta t} \\
+/// \end{align*}
+/// ```
+/// 
+/// where $` d `$ and $` d_R `$ are the number of translational and rotational
+/// degrees of freedom. Note that $` d_R `$ is determined by the number of
+/// non-zero components of the body's moment of inertia.
+/// 
+/// [fluctuation-dissipation theorem]: https://en.wikipedia.org/wiki/Fluctuation%E2%80%93dissipation_theorem
+/// 
+/// TODO: example
+pub struct Langevin<const N: usize, B, TT, TR = TT, G = f64, GR = [f64; N]>
+where
+    G: Gamma<BodyProperties = B>,
+    GR: GammaR<N, BodyProperties = B>,
+{
+    /// The time step size.
+    pub delta_t: f64,
+
+    /// Translational thermostat.
+    pub translational_thermostat: TT,
+
+    /// Rotational thermostat.
+    pub rotational_thermostat: TR,
+
+    /// Translational drag coefficient.
+    pub gamma: G,
+
+    /// Rotational drag coefficients.
+    pub gamma_r: GR,
+}
+
+impl<const N: usize, B, TT, TR, G, GR> Langevin<N, B, TT, TR, G, GR>
+where
+    G: Gamma<BodyProperties = B>,
+    GR: GammaR<N, BodyProperties = B>,
+{
+    /// Access the translational thermostat.
+    #[inline]
+    pub fn translational_thermostat(&self) -> &TT {
+        &self.translational_thermostat
+    }
+
+    /// Access the translational thermostat (mutable).
+    #[inline]
+    pub fn translational_thermostat_mut(&mut self) -> &mut TT {
+        &mut self.translational_thermostat
+    }
+
+    /// Access the rotational thermostat.
+    #[inline]
+    pub fn rotational_thermostat(&self) -> &TR {
+        &self.rotational_thermostat
+    }
+
+    /// Access the rotational thermostat (mutable).
+    #[inline]
+    pub fn rotational_thermostat_mut(&mut self) -> &mut TR {
+        &mut self.rotational_thermostat
+    }
+
+    /// Access the rotational thermostat.
+    #[inline]
+    pub fn gamma(&self) -> &G {
+        &self.gamma
+    }
+
+    /// Access the rotational thermostat (mutable).
+    #[inline]
+    pub fn gamma_mut(&mut self) -> &mut G {
+        &mut self.gamma
+    }
+
+    /// Access the rotational thermostat.
+    #[inline]
+    pub fn gamma_r(&self) -> &GR {
+        &self.gamma_r
+    }
+
+    /// Access the rotational thermostat (mutable).
+    #[inline]
+    pub fn gamma_r_mut(&mut self) -> &mut GR {
+        &mut self.gamma_r
+    }
+}
+
+/// Builder that constructs [`Langevin`].
+///
+/// Call [`Langevin::builder`] to start building a new [`Langevin`].
+pub struct LangevinBuilder<const N: usize, B, TT, TR = TT, G: Gamma<BodyProperties = B> = f64, GR: GammaR<N, BodyProperties = B> = [f64; N]> {
+    /// The time step size.
+    delta_t: f64,
+
+    /// Translational thermostat.
+    translational_thermostat: TT,
+
+    /// Rotational thermostat.
+    rotational_thermostat: TR,
+
+    /// Translational drag coefficient.
+    gamma: G,
+
+    /// Rotational drag coefficients.
+    gamma_r: GR,
+}
+
+
+impl<const N: usize, B, TT, TR, G, GR> LangevinBuilder<N, B, TT, TR, G, GR>
+where
+    G: Gamma<BodyProperties = B>,
+    GR: GammaR<N, BodyProperties = B>,
+{
+    /// Set the thermostat that applies to the translational degrees of freedom.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use hoomd_md::{method::Langevin, thermostat::Bussi};
+    ///
+    /// let delta_t = 0.001;
+    /// let constant_volume = Langevin::builder(delta_t)
+    ///     .translational_thermostat(Bussi::default())
+    ///     .build();
+    /// ```
+    #[inline]
+    pub fn translational_thermostat<T>(
+        self,
+        translational_thermostat: T,
+    ) -> LangevinBuilder<N, B, T, TR, G, GR> {
+        LangevinBuilder {
+            delta_t: self.delta_t,
+            translational_thermostat,
+            rotational_thermostat: self.rotational_thermostat,
+            gamma: self.gamma,
+            gamma_r: self.gamma_r
+        }
+    }
+
+    /// Set the thermostat that applies to the rotational degrees of freedom.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use hoomd_md::{method::Langevin, thermostat::Bussi};
+    ///
+    /// let delta_t = 0.001;
+    /// let langevin = Langevin::builder(delta_t)
+    ///     .rotational_thermostat(Bussi::default())
+    ///     .build();
+    /// ```
+    #[inline]
+    pub fn rotational_thermostat<T>(
+        self,
+        rotational_thermostat: T,
+    ) -> LangevinBuilder<N, B, TT, T, G, GR> {
+        LangevinBuilder {
+            delta_t: self.delta_t,
+            translational_thermostat: self.translational_thermostat,
+            rotational_thermostat,
+            gamma: self.gamma,
+            gamma_r: self.gamma_r
+        }
+    }
+
+    /// Set the thermostat that applies to both translational and rotational degrees of freedom.
+    ///
+    /// The given thermostat is cloned. The translational and rotational thermostats evolve
+    /// independently.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use hoomd_md::{method::Langevin, thermostat::Bussi};
+    ///
+    /// let delta_t = 0.001;
+    /// let langevin = Langevin::builder(delta_t)
+    ///     .thermostat(Bussi::default())
+    ///     .build();
+    /// ```
+    #[inline]
+    pub fn thermostat<T: Clone>(
+        self,
+        thermostat: T
+    ) -> LangevinBuilder<N, B, T, T, G, GR> {
+        LangevinBuilder {
+            delta_t: self.delta_t,
+            translational_thermostat: thermostat.clone(),
+            rotational_thermostat: thermostat,
+            gamma: self.gamma,
+            gamma_r: self.gamma_r
+        }
+    }
+
+    /// Set the drag coefficient that applies to translational degrees of freedom.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use hoomd_md::{method::Langevin, thermostat::Bussi};
+    ///
+    /// let delta_t = 0.001;
+    /// let langevin = Langevin::builder(delta_t)
+    ///     .gamma(2.0)
+    ///     .build();
+    /// ```
+    #[inline]
+    pub fn gamma<T: Gamma<B>>(
+        self,
+        gamma: T
+    ) -> LangevinBuilder<N, B, TT, TR, T, GR> {
+        LangevinBuilder {
+            delta_t: self.delta_t,
+            translational_thermostat: self.translational_thermostat,
+            rotational_thermostat: self.rotational_thermostat,
+            gamma,
+            gamma_r: self.gamma_r
+        }
+    }
+
+    /// Set the drag coefficients that apply to the rotational degrees of freedom.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use hoomd_md::{method::Langevin, thermostat::Bussi};
+    ///
+    /// let delta_t = 0.001;
+    /// let langevin = Langevin::builder(delta_t)
+    ///     .gamma_r([2.0, 2.0, 2.0])
+    ///     .build();
+    /// ```
+    #[inline]
+    pub fn gamma_r<T: GammaR<N, B>>(
+        self,
+        gamma_r: T
+    ) -> LangevinBuilder<N, B, TT, TR, G, T> {
+        LangevinBuilder {
+            delta_t: self.delta_t,
+            translational_thermostat: self.translational_thermostat,
+            rotational_thermostat: self.rotational_thermostat,
+            gamma: self.gamma,
+            gamma_r
+        }
+    }
+
+    /// Complete building a new [`Langevin`].
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use hoomd_md::method::Langevin;
+    ///
+    /// let delta_t = 0.001;
+    /// let langevin = Langevin::builder(delta_t).build();
+    /// ```
+    #[inline]
+    pub fn build(self) -> Langevin<N, B, TT, TR, G, GR> {
+        Langevin {
+            delta_t: self.delta_t,
+            translational_thermostat: self.translational_thermostat,
+            rotational_thermostat: self.rotational_thermostat,
+            gamma: self.gamma,
+            gamma_r: self.gamma_r
+        }
+    }
+}
+
+impl<const N: usize, B> Langevin<N, B, NoThermostat, NoThermostat, f64, [f64; N]> {
+    #[inline]
+    /// Start building a new `Langevin`.
+    ///
+    /// The default builder uses the given value for `delta_t` and [`NoThermostat`]
+    /// for both the translational and rotational thermostats. Call zero or more
+    /// of the [`LangevinBuilder`] methods to set the thermostats.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use hoomd_md::method::Langevin;
+    ///
+    /// let delta_t = 0.001;
+    /// let constant_volume = Langevin::builder(delta_t).build();
+    /// ```
+    /// [`NoThermostat`]: crate::thermostat::NoThermostat
+    pub fn builder(
+        delta_t: f64,
+    ) -> LangevinBuilder<N, B, NoThermostat, NoThermostat, f64, [f64; N]> {
+        LangevinBuilder::<N, B, NoThermostat, NoThermostat, f64, [f64; N]> {
+            delta_t,
+            translational_thermostat: NoThermostat,
+            rotational_thermostat: NoThermostat,
+            gamma: 1.0,
+            gamma_r: [1.0_f64; N]
+        }
+    }
+}
--- hoomd-md/src/method/mod.rs
+++ hoomd-md/src/method/mod.rs
@@ -5,3 +5,63 @@
 
 mod constant_volume;
 pub use constant_volume::{ConstantVolume, ConstantVolumeBuilder};
+
+mod langevin;
+pub use langevin::{Langevin, LangevinBuilder};
+
+/// The translational drag coefficient.
+/// 
+/// `Gamma` describes a type that provides a float representing $` \gamma `$,
+/// the translational drag coefficient used in [`Langevin`] and [`Brownian`]
+/// integration. Implement this trait on a new type to assign different drag
+/// coefficients to different sites.
+pub trait Gamma {
+    type BodyProperties;
+
+    /// Access the value for a site.
+    fn value(&self, site_properties: &Self::BodyProperties) -> f64;
+
+    /// Access the value for a site (mutable).
+    fn value_mut(&mut self, site_properties: &Self::BodyProperties) -> &mut f64;
+}
+
+/// The rotational drag coefficients.
+/// 
+/// `GammaR` describes a type that provides an array of N floats representing
+/// $` \gamma_R `$, the rotational drag coefficients for the rotational degrees
+/// of freedom used in [`Langevin`] and [`Brownian`] integration. Implement this
+/// trait on a new type to assign different sets of drag coefficients to
+/// different sites.
+pub trait GammaR<const N: usize> {
+    type BodyProperties;
+
+    /// Access the value for a site
+    fn value(&self, site_properties: &Self::BodyProperties) -> [f64; N];
+
+    /// Access the value for a site (mutable).
+    fn value_mut(&mut self, site_properties: &Self::BodyProperties) -> &mut [f64; N];
+}
+
+impl Gamma for f64 {
+    type BodyProperties = usize;
+    
+    fn value(&self, _: &Self::BodyProperties) -> f64 {
+        *self
+    }
+
+    fn value_mut(&mut self, _: &Self::BodyProperties) -> &mut f64 {
+        self
+    }
+}
+
+impl<const N: usize> GammaR<N> for [f64; N] {
+    type BodyProperties = usize;
+    
+    fn value(&self, _: &Self::BodyProperties) -> [f64; N] {
+        *self
+    }
+    
+    fn value_mut(&mut self, _: &Self::BodyProperties) -> &mut [f64; N] {
+        self
+    }
+}
