0f6342c06ca3a7dd3407cf15b3e5fe7b69b34f06
Joseph Burkhart <61951318+josephburkhart@users.noreply.github.com>
2026-07-22 13:11:01 -0400

    Refactor Gamma and GammaR traits so that users implement them on body properties

--- hoomd-md/src/method/mod.rs
+++ hoomd-md/src/method/mod.rs
@@ -15,27 +15,14 @@ pub use langevin::{Langevin, LangevinBuilder};
 /// 
 /// `Gamma` describes a type that provides a float representing $` \gamma `$,
 /// the translational drag coefficient used in [`Langevin`] and [`Brownian`]
-/// integration. Implement this trait on a new type to assign different drag
-/// coefficients to different bodies.
+/// integration. Implement this trait on a body properties object to assign a
+/// specific drag coefficient to a specific body.
 /// 
 /// The generic type names are:
 /// * `B`: The [`Body::properties`](hoomd_microstate::Body) type.
-pub trait Gamma<B: Momentum> {
-    /// Access the value for a site.
-    fn value(&self, body_properties: &B) -> f64;
-
-    /// Access the value for a site (mutable).
-    fn value_mut(&mut self, body_properties: &B) -> &mut f64;
-}
-
-impl<B: Momentum> Gamma<B> for f64 {    
-    fn value(&self, _: &B) -> f64 {
-        *self
-    }
-
-    fn value_mut(&mut self, _: &B) -> &mut f64 {
-        self
-    }
+pub trait Gamma {
+    /// Access the drag coefficient for the body.
+    fn gamma(&self) -> f64;
 }
 
 /// The rotational drag coefficients.
@@ -43,41 +30,15 @@ impl<B: Momentum> Gamma<B> for f64 {
 /// `GammaR` describes a type that provides an array of N floats representing
 /// $` \gamma_R `$, the rotational drag coefficients for the rotational degrees
 /// of freedom used in [`Langevin`] and [`Brownian`] integration. Implement this
-/// trait on a new type to assign different sets of drag coefficients to
-/// different bodies.
+/// trait on a body properties object to assign specific drag coefficients to
+/// specific bodies.
+/// 
 /// The generic type names are:
 /// * `B`: The [`Body::properties`](hoomd_microstate::Body) type.
-pub trait GammaR<B: AngularMomentum> {
+pub trait GammaR {
     /// The type containing the rotational drag coefficient(s).
     type GammaR;
 
     /// Access the value for a site
-    fn value(&self, body_properties: &B) -> &Self::GammaR;
-
-    /// Access the value for a site (mutable).
-    fn value_mut(&mut self, body_properties: &B) -> &mut Self::GammaR;
-}
-
-impl<B: AngularMomentum<AngularMomentum = f64>> GammaR<B> for f64 {
-    type GammaR = f64;
-
-    fn value(&self, _: &B) -> &f64 {
-        self
-    }
-    
-    fn value_mut(&mut self, _: &B) -> &mut f64 {
-        self
-    }
-}
-
-impl<B: AngularMomentum<AngularMomentum = Cartesian<3>>> GammaR<B> for [f64; 3] {
-    type GammaR = [f64; 3];
-
-    fn value(&self, _: &B) -> &[f64; 3] {
-        self
-    }
-    
-    fn value_mut(&mut self, _: &B) -> &mut [f64; 3] {
-        self
-    }
+    fn gamma_r(&self) -> &Self::GammaR;
 }
