ec5207a2d5f6edf1c08ab7c1ce5696585f12970c
janbridley <bradleyjenj@gmail.com>
2026-09-18 16:14:54 -0400

    3d surface mesh implementation

--- hoomd-geometry/src/shape/convex_surface_mesh_3d.rs
+++ hoomd-geometry/src/shape/convex_surface_mesh_3d.rs
@@ -3,20 +3,11 @@
 
 //! Convex polygon represented by vertices and edges.
 
+use crate::{ConvexHull, Error, Facet, shape::ConvexPolytope};
 use serde::{Deserialize, Serialize};
-use serde_with::serde_as;
 
 use hoomd_utility::valid::PositiveReal;
 use hoomd_vector::Cartesian;
-
-/// TODO
-#[serde_as]
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
-pub struct Facet<const N: usize> {
-    /// TODO
-    #[serde_as(as = "[_; N]")]
-    indices: [i32; N],
-}
 
 /// The vertices and edges that make up a convex polygon.
 ///
@@ -56,8 +47,48 @@ pub struct Facet<const N: usize> {
 pub struct ConvexSurfaceMesh3d {
     /// The vertices of the polyhedron.
     vertices: Vec<Cartesian<3>>,
-    /// .
-    facets: Vec<Facet<3>>,
+    /// The triangular facets of the polyhedron, as indices into `vertices`.
+    faces: Vec<Facet<3>>,
     /// The radius of a bounding sphere of the geometry.
     bounding_radius: PositiveReal,
+}
+
+impl ConvexSurfaceMesh3d {
+    /// Create a convex surface mesh from the convex hull of the given set of points.
+    ///
+    /// The resulting shape contains a subset of the given points, including only the
+    /// non-degenerate points on the convex hull, in the order of the input. The faces
+    /// of the result are the triangles of the hull, given as indices into its vertices.
+    ///
+    /// # Errors
+    ///
+    /// * [`Error::DegeneratePolytope`] when there are fewer than 4 non-coplanar points.
+    ///
+    /// # Example
+    /// ```
+    /// use hoomd_geometry::shape::ConvexSurfaceMesh3d;
+    ///
+    /// # fn main() -> Result<(), hoomd_geometry::Error> {
+    /// let tetrahedron = ConvexSurfaceMesh3d::from_point_set([
+    ///     [1.0, 1.0, 1.0].into(),
+    ///     [1.0, -1.0, -1.0].into(),
+    ///     [-1.0, 1.0, -1.0].into(),
+    ///     [-1.0, -1.0, 1.0].into(),
+    /// ])?;
+    /// # Ok(())
+    /// # }
+    /// ```
+    #[inline]
+    pub fn from_point_set<I>(points: I) -> Result<Self, Error>
+    where
+        I: IntoIterator<Item = Cartesian<3>>,
+    {
+        let (vertices, faces) = Cartesian::<3>::convex_hull(points)?;
+
+        Ok(Self {
+            bounding_radius: ConvexPolytope::<3>::bounding_radius(&vertices),
+            vertices,
+            faces,
+        })
+    }
 }
